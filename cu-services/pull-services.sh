dbaeb60917905d2f6af1d314d6122b81e75d0749
claire <c.wahl@treeptik.fr>
2015-09-24 16:30:27 +0200

    FEAT- les images sont tirés depuis le docker hub

---
+++ cu-services/pull-services.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LOG_FILE=/home/admincu/cloudunit/cu-services/pull-log
+CACHE=false
+
+rm $LOG_FILE
+
+while read line
+do
+	image=`echo $line | cut -d" " -f 1`
+	echo -e "\nPulling image $image."
+	docker pull $image 
+	docker images | grep $image
+	return=$?
+	if [ "$return" -eq "0" ]; then
+		echo -e "\nThe docker image $image has been correctly pulled.\n" >> $LOG_FILE
+	else
+		echo -e "\nPROBLEM: the docker image $image has not been pulled!\n" >> $LOG_FILE
+	fi
+done < cu-images
