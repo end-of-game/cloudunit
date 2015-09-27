7f961b073e8126964d9ddf1e87631538dc4c544b
nicolas <n.muller@treeptik.fr>
2015-09-27 18:58:53 +0200

    Fix : Security ADD CRSF Filter

--- cu-manager/src/main/java/fr/treeptik/cloudunit/config/SecurityConfiguration.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/config/SecurityConfiguration.java
@@ -23,10 +23,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -95,9 +107,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.logout().logoutUrl("/user/logout")
 				.logoutSuccessHandler(ajaxLogoutSuccessHandler)
 				.deleteCookies("JSESSIONID").invalidateHttpSession(true)
-				.permitAll().and().csrf().disable();
+				.permitAll().and().csrf()
+                    .csrfTokenRepository(csrfTokenRepository()).and()
+                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
 
-		http.authorizeRequests()
+        http.authorizeRequests()
 				// .antMatchers("/resources/**").permitAll()
 				.antMatchers("/application/**")
 				.hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
@@ -123,9 +137,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/user/check", "/nopublic/**").permitAll().and()
 				.exceptionHandling()
 				.authenticationEntryPoint(authenticationEntryPoint);
-		// .and().headers().cacheControl().frameOptions();
-
 	}
+
+    private Filter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain filterChain)
+                    throws ServletException, IOException {
+                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                        .getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null || token != null
+                            && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
 
 	@Bean
 	public CustomPasswordEncoder passwordEncoder() {
