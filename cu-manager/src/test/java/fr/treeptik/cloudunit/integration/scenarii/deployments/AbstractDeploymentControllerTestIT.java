341f19e8720b78e99b4d1ad091a5b1ef80c387e3
nicolas <n.muller@treeptik.fr>
2015-10-01 18:16:09 +0200

    Add Test Integration for Deployment

--- cu-manager/src/test/java/fr/treeptik/cloudunit/integration/scenarii/deployments/AbstractDeploymentControllerTestIT.java
+++ cu-manager/src/test/java/fr/treeptik/cloudunit/integration/scenarii/deployments/AbstractDeploymentControllerTestIT.java
@@ -1,27 +1,16 @@
 package fr.treeptik.cloudunit.integration.scenarii.deployments;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.Random;
-
-import javax.inject.Inject;
-import javax.servlet.Filter;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import fr.treeptik.cloudunit.exception.ServiceException;
+import fr.treeptik.cloudunit.initializer.CloudUnitApplicationContext;
+import fr.treeptik.cloudunit.model.User;
+import fr.treeptik.cloudunit.service.UserService;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
@@ -47,23 +36,27 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import fr.treeptik.cloudunit.exception.ServiceException;
-import fr.treeptik.cloudunit.initializer.CloudUnitApplicationContext;
-import fr.treeptik.cloudunit.model.User;
-import fr.treeptik.cloudunit.service.UserService;
-import fr.treeptik.cloudunit.utils.RestUtils;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import java.io.*;
+import java.net.URL;
+import java.util.Random;
 
-@RunWith( SpringJUnit4ClassRunner.class )
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration( classes = { CloudUnitApplicationContext.class, MockServletContext.class } )
-@FixMethodOrder( MethodSorters.NAME_ASCENDING )
-@ActiveProfiles( "integration" )
+@ContextConfiguration(classes = {CloudUnitApplicationContext.class, MockServletContext.class})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ActiveProfiles("integration")
 public abstract class AbstractDeploymentControllerTestIT
 
 {
     protected String release;
 
-    private final Logger logger = LoggerFactory.getLogger( AbstractDeploymentControllerTestIT.class );
+    private final Logger logger = LoggerFactory.getLogger(AbstractDeploymentControllerTestIT.class);
 
     @Autowired
     private WebApplicationContext context;
@@ -86,79 +79,87 @@ public abstract class AbstractDeploymentControllerTestIT
     private static String applicationName;
 
     @BeforeClass
-    public static void initEnv()
-    {
-        applicationName = "App" + new Random().nextInt( 1000 );
+    public static void initEnv() {
+        applicationName = "App" + new Random().nextInt(1000);
     }
 
     @Before
-    public void setup()
-    {
-        logger.info( "setup" );
+    public void setup() {
+        logger.info("setup");
 
-        this.mockMvc = MockMvcBuilders.webAppContextSetup( context ).addFilters( springSecurityFilterChain ).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).addFilters(springSecurityFilterChain).build();
 
         User user = null;
-        try
-        {
-            user = userService.findByLogin( "johndoe" );
-        }
-        catch ( ServiceException e )
-        {
-            logger.error( e.getLocalizedMessage() );
+        try {
+            user = userService.findByLogin("johndoe");
+        } catch (ServiceException e) {
+            logger.error(e.getLocalizedMessage());
         }
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken( user.getLogin(), user.getPassword() );
-        Authentication result = authenticationManager.authenticate( authentication );
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword());
+        Authentication result = authenticationManager.authenticate(authentication);
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication( result );
+        securityContext.setAuthentication(result);
         session = new MockHttpSession();
-        session.setAttribute( HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext );
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
 
-        if ( !isAppCreated )
-        {
-            try
-            {
-                logger.info( "Create Tomcat server" );
+        if (!isAppCreated) {
+            try {
+                logger.info("Create Tomcat server");
                 String jsonString =
                     "{\"applicationName\":\"" + applicationName + "\", \"serverName\":\"" + release + "\"}";
                 ResultActions resultats =
-                    mockMvc.perform( post( "/application" ).session( session ).contentType( MediaType.APPLICATION_JSON ).content( jsonString ) );
-                resultats.andExpect( status().isOk() );
+                    mockMvc.perform(post("/application").session(session).contentType(MediaType.APPLICATION_JSON).content(jsonString));
+                resultats.andExpect(status().isOk());
                 isAppCreated = true;
-            }
-            catch ( Exception e )
-            {
-                logger.error( e.getMessage() );
+            } catch (Exception e) {
+                logger.error(e.getMessage());
             }
         }
     }
 
-    @Test( timeout = 30000 )
-    public void test061_DeploySimpleApplicationTest()
-        throws Exception
-    {
-        logger.info( "Deploy an helloworld application" );
-        URL url = new URL( "https://web-actions.googlecode.com/files/helloworld.war" );
+    @Test(timeout = 60000)
+    public void test10_DeploySimpleApplicationTest()
+        throws Exception {
+        logger.info("Deploy an helloworld application");
+        URL url = new URL("https://github.com/Treeptik/CloudUnit/releases/download/0.9/helloworld.war");
         InputStream input = url.openStream();
-        File file = new File( "helloworld.war" );
-        OutputStream outputStream = new FileOutputStream( file );
+        File file = new File("helloworld.war");
+        OutputStream outputStream = new FileOutputStream(file);
         int read = 0;
         byte[] bytes = new byte[1024];
 
-        while ( ( read = input.read( bytes ) ) != -1 )
-        {
-            outputStream.write( bytes, 0, read );
+        while ((read = input.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, read);
         }
         MockMultipartFile fileToDeploy =
-            new MockMultipartFile( "file", file.getName(), "multipart/form-data", new FileInputStream( file ) );
+            new MockMultipartFile("file", file.getName(), "multipart/form-data", new FileInputStream(file));
 
-        ResultActions resultats =
-            this.mockMvc.perform( MockMvcRequestBuilders.fileUpload( "/application/" + applicationName + "/deploy" ).file( fileToDeploy ).session( session ).contentType( MediaType.MULTIPART_FORM_DATA ) ).andDo( print() );
-        resultats.andExpect( status().is2xxSuccessful() );
-//        String response = RestUtils.sendGetCommand( "http://" + applicationName + "-johndoe-treeptik.cloudunit.dev" );
-//        logger.info( "http response= " + response );
-//        Assert.assertTrue( response.contains( "Helloworld" ) );
+        ResultActions resultats = mockMvc.perform(MockMvcRequestBuilders.fileUpload("/application/" + applicationName + "/deploy").file(fileToDeploy).session(session).contentType(MediaType.MULTIPART_FORM_DATA)).andDo(print());
+        resultats.andExpect(status().is2xxSuccessful());
 
+        boolean found = false;
+        int counter = 0;
+        while (counter++ < 3) {
+            String urlToCall = "http://" + applicationName.toLowerCase() + "-johndoe-admin.cloudunit.dev";
+            System.out.println(urlToCall);
+            HttpGet request = new HttpGet(urlToCall);
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            String entityContents = EntityUtils.toString(entity);
+
+            logger.info("http response= " + entityContents);
+            if (entityContents.contains("CloudUnit PaaS")) {
+                found = true;
+                break;
+            }
+            Thread.sleep(3000);
+        }
+        if (found) {
+            Assert.assertTrue("Application is right deployed", true);
+        } else {
+            Assert.assertTrue("Application is not correctly deployed", false);
+        }
     }
 }
