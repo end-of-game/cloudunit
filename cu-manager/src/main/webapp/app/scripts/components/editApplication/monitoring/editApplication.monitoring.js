b25a37d2fe42103c07aaf25e7429e02c11d963af
tomaka <htomaka@treeptik.fr>
2016-01-14 10:57:07 +0100

    REFACTO - moved data transformation to model

--- cu-manager/src/main/webapp/app/scripts/components/editApplication/monitoring/editApplication.monitoring.js
+++ cu-manager/src/main/webapp/app/scripts/components/editApplication/monitoring/editApplication.monitoring.js
@@ -54,10 +54,12 @@
     vm.getContainers = getContainers;
     vm.stats = {};
     vm.cpuUsage = [];
+    vm.monitoringService = MonitoringService;
 
     vm.chartOptions = {
       scaleMinSpace: 300
-    }
+    };
+
     vm.chartPieOptions = {
       donut: true,
       donutWidth: 20,
@@ -66,230 +68,28 @@
     };
 
     $scope.$on ( '$destroy', function () {
-      $interval.cancel ( timer );
+      vm.monitoringService.stopPollStats();
     } );
 
     getContainers ();
 
     function getContainers ( selectedContainer ) {
+      vm.isLoading = true;
       return ApplicationService.listContainers ( $stateParams.name )
         .then ( function onGetContainersComplete ( containers ) {
           vm.containers = containers;
           vm.myContainer = selectedContainer || containers[0];
-          vm.isLoading = false;
 
-          MonitoringService.getMachineInfo ().then ( function (machineInfo) {
-            updateStats ( vm.myContainer.name, machineInfo );
-            if ( !timer ) {
-              timer = pollStats ( vm.myContainer.name, machineInfo );
-            }
-          } ).catch ( function () {
-            if ( timer ) {
-              $interval.cancel ( timer );
-            }
-          } );
+          console.log(vm.myContainer);
+          vm.monitoringService.initStats ( vm.myContainer.name ).then ( function () {
+            vm.stats = vm.monitoringService.stats;
+          } )
         } )
         .catch ( function onGetContainersError ( reason ) {
           console.error ( reason ); //debug
         } );
     }
 
-    function updateStats ( containerName, machineInfo ) {
-      MonitoringService.getStats ( containerName ).then ( function ( stats ) {
-        vm.cpuTotalUsage = cpuTotalUsage ( stats );
-        vm.cpuUsageBreakdown = cpuUsageBreakdown(stats);
-        vm.cpuPerCoreUsage = cpuPerCoreUsage(machineInfo, stats);
-        vm.memoryUsage = memoryUsage(stats);
-        vm.networkUsage = networkUsage(stats);
-        vm.networkErrors = networkErrors(stats);
-        vm.cpuLoad = cpuLoad(stats, machineInfo);
-      } );
-    }
-
-    var oneMegabyte = 1024 * 1024;
-
-    function getInterval ( current, previous ) {
-      var cur = new Date ( current );
-      var prev = new Date ( previous );
-
-      // ms -> ns.
-      return (cur.getTime () - prev.getTime ()) * 1000000;
-    }
-
-    // Checks if the specified stats include the specified resource.
-    function hasResource ( stats, resource ) {
-      return stats.stats.length > 0 && stats.stats[0][resource];
-    }
-
-
-    function cpuTotalUsage ( stats ) {
-      if ( stats.spec.has_cpu && !hasResource ( stats, "cpu" ) ) {
-        return;
-      }
-      var data = {
-        legends: ['Total'],
-        labels: [],
-        series: [[]]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var intervalInNs = getInterval ( cur.timestamp, prev.timestamp );
-
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( (cur.cpu.usage.total - prev.cpu.usage.total) / intervalInNs );
-      }
-      return data;
-    }
-
-    function cpuPerCoreUsage ( machineInfo, stats ) {
-      if ( stats.spec.has_cpu && !hasResource ( stats, "cpu" ) ) {
-        return;
-      }
-
-      var data = {
-        legends: [],
-        labels: [],
-        series: []
-      };
-
-      for ( var i = 0; i < machineInfo.num_cores; i++ ) {
-        data.legends.push ( "Core " + i );
-        data.series[i] = [];
-      }
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var intervalInNs = getInterval ( cur.timestamp, prev.timestamp );
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        for ( var j = 0; j < machineInfo.num_cores; j++ ) {
-          data.series[j].push ( (cur.cpu.usage.per_cpu_usage[j] - prev.cpu.usage.per_cpu_usage[j]) / intervalInNs );
-        }
-      }
-      return data;
-    }
-
-    function cpuUsageBreakdown ( stats ) {
-      if ( stats.spec.has_cpu && !hasResource ( stats, "cpu" ) ) {
-        return;
-      }
-
-      var data = {
-        legends: ['User', 'Kernel'],
-        labels: [],
-        series: [[], []]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var intervalInNs = getInterval ( cur.timestamp, prev.timestamp );
-
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( (cur.cpu.usage.user - prev.cpu.usage.user) / intervalInNs );
-        data.series[1].push ( (cur.cpu.usage.system - prev.cpu.usage.system) / intervalInNs );
-      }
-      return data;
-
-    }
-
-    function cpuLoad ( stats, machineInfo ) {
-      var data = {
-        legends: ['Memory'],
-        labels: [],
-        series: [[]]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var cpuUsage = 0;
-
-        var rawUsage = cur.cpu.usage.total - prev.cpu.usage.total;
-        var intervalInNs = getInterval ( cur.timestamp, prev.timestamp );
-        cpuUsage = Math.round ( ((rawUsage / intervalInNs) / machineInfo.num_cores) * 100 );
-        if ( cpuUsage > 100 ) {
-          cpuUsage = 100;
-        }
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( cpuUsage );
-      }
-
-      return data;
-    }
-
-    function memoryUsage ( stats ) {
-      if ( stats.spec.has_memory && !hasResource ( stats, "memory" ) ) {
-        return;
-      }
-
-      var data = {
-        legends: ['Total', 'Hot'],
-        labels: [],
-        series: [[], []]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( cur.memory.usage / oneMegabyte );
-        data.series[1].push ( cur.memory.working_set / oneMegabyte );
-      }
-      return data;
-    }
-
-    function networkUsage ( stats ) {
-      if ( stats.spec.has_network && !hasResource ( stats, "network" ) ) {
-        return;
-      }
-
-      var data = {
-        legends: ['Tx', 'Rx'],
-        labels: [],
-        series: [[], []]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var intervalInSec = getInterval ( cur.timestamp, prev.timestamp ) / 1000000000;
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( (cur.network.tx_bytes - prev.network.tx_bytes) / intervalInSec );
-        data.series[1].push ( (cur.network.rx_bytes - prev.network.rx_bytes) / intervalInSec );
-      }
-      return data;
-    }
-
-    function networkErrors ( stats ) {
-      if ( stats.spec.has_network && !hasResource ( stats, "network" ) ) {
-        return;
-      }
-
-      var data = {
-        legends: ['Tx', 'Rx'],
-        labels: [],
-        series: [[], []]
-      };
-
-      for ( var i = 1; i < stats.stats.length; i++ ) {
-        var cur = stats.stats[i];
-        var prev = stats.stats[i - 1];
-        var intervalInSec = getInterval ( cur.timestamp, prev.timestamp ) / 1000000000;
-        data.labels.push ( moment ( cur.timestamp ).format ( 'HH:MM:ss' ) );
-        data.series[0].push ( (cur.network.tx_errors - prev.network.tx_errors) / intervalInSec );
-        data.series[1].push ( (cur.network.rx_errors - prev.network.rx_errors) / intervalInSec );
-      }
-      return data;
-
-    }
-
-
-    function pollStats ( containerName, machineInfo ) {
-      return $interval ( function () {
-        updateStats ( containerName, machineInfo );
-      }, 2000 )
-    }
   }
 }) ();
 
