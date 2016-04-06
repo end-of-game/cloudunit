7e9f69fbc23941a6687682fec6b4a446fd1be0d4
nicolas <zepouet@gmail.com>
2016-04-06 20:52:01 +0200

    Add Dockerfile to public build CloudUnit

---
+++ cu-manager/dockerhub/create_admin_user.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -f /.tomcat_admin_created ]; then
+	echo "Tomcat 'admin' user already created"
+		exit 0
+fi
+
+#generate password
+PASS=${TOMCAT_PASS:-$(cat /dev/urandom| tr -dc 'a-zA-Z0-9' | fold -w 10| head -n 1)}
+_word=$( [ ${TOMCAT_PASS} ] && echo "preset" || echo "random" )
+
+echo "=> Creating and admin user with a ${_word} password in Tomcat"
+sed -i -r 's/<\/tomcat-users>//' ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="manager-gui"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="manager-script"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="manager-jmx"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="admin-gui"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="admin-script"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo "<user username=\"admin\" password=\"${PASS}\" roles=\"manager-gui,manager-script,manager-jmx,admin-gui, admin-script\"/>" >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '</tomcat-users>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo "=> Done!"
+touch ${CATALINA_HOME}/scripts/.tomcat_admin_created
+
+echo "========================================================================"
+echo "You can now configure to this Tomcat server using:"
+echo ""
+echo "    admin:${PASS}"
+echo ""
+echo "========================================================================"
