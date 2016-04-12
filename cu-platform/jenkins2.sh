bee387f8d7c242f34b2c9fe3d15595fbefda8ba3
nicolas <zepouet@gmail.com>
2016-04-12 16:10:41 +0200

    Update version jenkins

--- cu-platform/jenkins2.sh
+++ cu-platform/jenkins2.sh
@@ -3,7 +3,7 @@
 docker run  --name jenkins2 \
             -d -p 8080:8080 -p 50000:50000 \
             -v /var/jenkins_home \
-            jenkinsci/jenkins:2.0-alpha-3
+            jenkinsci/jenkins:2.0-rc1
 
 ## --env JAVA_OPTS=-Dhudson.footerURL=http://mycompany.com \
 
