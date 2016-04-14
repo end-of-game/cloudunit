1593a3ffbf2daf61d917bbf29303ee10fdb01d5f
nicolas <zepouet@gmail.com>
2016-04-14 17:55:16 +0200

    Update Doc

--- cu-production/plugins.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-vagrant plugin install vagrant-reload
-vagrant plugin install vagrant-vbguest
--- cu-production/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$(uname)" == "Darwin" ]; then
-    vagrant up mac
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    vagrant up linux
-fi
