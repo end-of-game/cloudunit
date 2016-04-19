eddf1223139f3ec8edefef44b4d335e75db9a1d4
nicolas <zepouet@gmail.com>
2016-04-19 18:24:41 +0200

    Persist Jenkins2 configuration

--- cu-platform/jenkins2.sh
+++ cu-platform/jenkins2.sh
@@ -2,7 +2,7 @@
 
 docker run  --name jenkins2 \
             -d -p 9080:8080 -p 50000:50000 \
-            -v /var/jenkins_home \
+            -v /home/vagrant/docker:/var/jenkins_home \
             jenkinsci/jenkins:2.0-rc-1
 
 ## --env JAVA_OPTS=-Dhudson.footerURL=http://mycompany.com \
