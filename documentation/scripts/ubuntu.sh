1a21567bc9b42e5a808584be4728c1cb6a7e10ea
GuillaumeUnice <guillaume.unice@gmail.com>
2016-04-19 15:29:16 +0200

    Update ubuntu.sh

--- documentation/scripts/ubuntu.sh
+++ documentation/scripts/ubuntu.sh
@@ -1,37 +1,85 @@
 #!/bin/bash
 
+###### Compass #######
 sudo apt-get -y install ruby-full
 sudo gem install compass
 
-sudo apt-get -y install git
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get -y update
-sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install oracle-java8-set-default
 
+sudo apt-get -y install git
+
+###### Java #######
+if type -p java; then
+	echo found java executable in PATH
+	_java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+	echo found java executable in JAVA_HOME     
+	_java="$JAVA_HOME/bin/java"
+else
+	echo "no java"
+	exit 0
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" < "1.8" ]]; then
+	echo version is less than 1.8
+	exit 0
+    fi
+fi
+
+###### Node #######
 curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
 sudo apt-get -y install nodejs
-
 NODE=$(node -v | cut -c1-2) 
 VERSION='v5'
-
 if [ $NODE != $VERSION ]
 then
 	echo 'Node can not be installed'
 	exit 0
 fi
 
-wget http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip apache-maven-3.3.9-bin.zip -d $HOME/cloudunit/
-export PATH=$HOME/apache-maven-3.x.y/bin:$PATH
+###### Maven #######
+sudo apt-get -y remove maven && sudo apt-get -y update && sudo apt-get -y install maven
 
-wget http://download.virtualbox.org/virtualbox/debian vivid contrib
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get -y update
-sudo apt-get -y install virtualbox-5.0
+###### VirtualBox #######
+ISVIRTUALBOX = $(hash virtualbox)
+if($ISVIRTUALBOX)
+then
+	wget http://download.virtualbox.org/virtualbox/debian vivid contrib
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+	sudo apt-get -y update
+	sudo apt-get -y install virtualbox-5.0
+else
+	VIRTUALBOX = $(virtualbox --help | head -n 1 | awk '{print $NF}')
+	echo $VIRTUALBOX
+	if($VIRTUALBOX[0] < 5)
+	then
+		echo 'VirtualBox is less than 5.x'
+		wget http://download.virtualbox.org/virtualbox/debian vivid contrib
+		wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+		sudo apt-get -y update
+		sudo apt-get -y install virtualbox-5.0
+	fi
+fi
 
-wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
-sudo dpkg -i vagrant_1.8.1_x86_64.deb
+###### Vagrant #######
+ISVAGRANT = $(hash vagrant)
+if($ISVAGRANT)
+then
+	wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+	sudo dpkg -i vagrant_1.8.1_x86_64.deb
+else
+	VAGRANT = $(vagrant -v | tail -c 6)
+	if($VAGRANT[0] <= 1 && $VAGRANT[2] < 8)
+	then
+		wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+		sudo dpkg -i vagrant_1.8.1_x86_64.deb
+	fi
+fi
+
+
+source $HOME/.bashrc
 
 sudo apt-get -y install dnsmasq
 ADDRESS="address=/.cloudunit.dev/192.168.50.4"
@@ -48,9 +96,8 @@ sudo npm install -g grunt grunt-cli bower
 cd $HOME/cloudunit/cu-manager/src/main/webapp && sudo npm install
 
 cd $HOME/cloudunit/cu-vagrant
-vagrant up
+sudo vagrant up
 vagrant provision 
 
-rm -f vagrant_1.8.1_x86_64.deb
-rm -f debian
-rm -f apache-maven-3.3.9-bin.zip
+#rm -f vagrant_1.8.1_x86_64.deb
+#rm -f debian
