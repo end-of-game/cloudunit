a4461b52e48edc1f38341aea8b9cdbb3c9f5fcc4
Kan23 <romain.lelievre93@gmail.com>
2016-04-20 11:49:40 +0200

    Add functions script installation ubuntu

--- documentation/scripts/ubuntu-15.04.sh
+++ documentation/scripts/ubuntu-15.04.sh
@@ -1,87 +1,97 @@
 #!/bin/bash
 
-###### Java #######
-if type -p java; then
-	echo found java executable in PATH
-	_java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-	echo found java executable in JAVA_HOME     
-	_java="$JAVA_HOME/bin/java"
-else
-	echo "no java"
-	exit 0
-fi
-
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ "$version" < "1.8" ]]; then
-	echo version is less than 1.8
-	exit 0
-    fi
-fi
-
-###### Node #######
-#curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
-#sudo apt-get -y install nodejs
-NODEVER=$(node -v | cut -c2)
-if (( $NODEVER < 5 ))
-then
-	echo 'Node can not be installed'
-	exit 0
-fi
-
-###### Maven #######
-ISMAVEN=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $4}')
-
-if [ "$ISMAVEN" != "installed" ]
-then 
-	sudo apt-get -y update && sudo apt-get -y install maven
-else
-	MAVENVER=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $2}' | colrm 2)
-	if (( $MAVENVER < 3 ))
-	then
-		sudo apt-get -y remove maven && sudo apt-get -y update && sudo apt-get -y install maven
+function install_java {
+	if type -p java; then
+		echo found java executable in PATH
+		_java=java
+	elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+		echo found java executable in JAVA_HOME     
+		_java="$JAVA_HOME/bin/java"
+	else
+		echo "no java"
+		exit 0
 	fi
-fi
 
-###### VirtualBox #######
-ISVIRTUALBOX=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $4}')
+	if [[ "$_java" ]]; then
+	    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+	    echo version "$version"
+	    if [[ "$version" < "1.8" ]]; then
+		echo version is less than 1.8
+		exit 0
+	    fi
+	fi
+}
 
-if [ "$ISVIRTUALBOX" != "installed" ]
-then 
-	wget http://download.virtualbox.org/virtualbox/debian vivid contrib
-	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-	sudo apt-get -y update
-	sudo apt-get -y install virtualbox-5.0
-else
-	VIRTUALBOXVER=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $2}' | colrm 6)
-	if ( (( "$(echo $VIRTUALBOXVER | cut -c1)" <= 5 )) && (( "$(echo $VIRTUALBOXVER | cut -c3)" <= 0 )) &&  (( "$(echo $VIRTUALBOXVER | cut -c5)" < 4)) )
+function install_node {
+	curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
+	sudo apt-get -y install nodejs
+	NODEVER=$(node -v | cut -c2)
+	if (( $NODEVER < 5 ))
 	then
+		echo 'Node can not be installed'
+		exit 0
+	fi
+}
+
+function install_maven {
+	ISMAVEN=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $4}')
+
+	if [ "$ISMAVEN" != "installed" ]
+	then 
+		sudo apt-get -y update && sudo apt-get -y install maven
+	else
+		MAVENVER=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $2}' | colrm 2)
+		if (( $MAVENVER < 3 ))
+		then
+			sudo apt-get -y remove maven && sudo apt-get -y update && sudo apt-get -y install maven
+		fi
+	fi
+}
+
+function install_virtualbox {
+	ISVIRTUALBOX=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $4}')
+
+	if [ "$ISVIRTUALBOX" != "installed" ]
+	then 
 		wget http://download.virtualbox.org/virtualbox/debian vivid contrib
 		wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 		sudo apt-get -y update
 		sudo apt-get -y install virtualbox-5.0
+	else
+		VIRTUALBOXVER=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $2}' | colrm 6)
+		if ( (( "$(echo $VIRTUALBOXVER | cut -c1)" <= 5 )) && (( "$(echo $VIRTUALBOXVER | cut -c3)" <= 0 )) &&  (( "$(echo $VIRTUALBOXVER | cut -c5)" < 4)) )
+		then
+			wget http://download.virtualbox.org/virtualbox/debian vivid contrib
+			wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+			sudo apt-get -y update
+			sudo apt-get -y install virtualbox-5.0
+		fi
 	fi
-fi
+}
 
-###### Vagrant #######
-ISVAGRANT=$(dpkg -s vagrant | grep -e Status | head -n 1 | awk '{print $4}')
+function install_vagrant {
+	ISVAGRANT=$(dpkg -s vagrant | grep -e Status | head -n 1 | awk '{print $4}')
 
-if [ "$ISVAGRANT" != "installed" ]
-then 
-	wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
-	sudo dpkg -i vagrant_1.8.1_x86_64.deb
-else
-	VAGRANTVER=$(dpkg -s vagrant | grep -e Version | head -n 1  | awk '{print $2}' | cut -c3-5)
-	if ( (( "$(echo $VAGRANTVER | cut -c1)" <= 1 )) && (( "$(echo $VAGRANTVER | cut -c3)" < 8 )) )
-	then
-		echo "Vagrant not install or not good version"		
+	if [ "$ISVAGRANT" != "installed" ]
+	then 
 		wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
 		sudo dpkg -i vagrant_1.8.1_x86_64.deb
+	else
+		VAGRANTVER=$(dpkg -s vagrant | grep -e Version | head -n 1  | awk '{print $2}' | cut -c3-5)
+		if ( (( "$(echo $VAGRANTVER | cut -c1)" <= 1 )) && (( "$(echo $VAGRANTVER | cut -c3)" < 8 )) )
+		then
+			echo "Vagrant not install or not good version"		
+			wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+			sudo dpkg -i vagrant_1.8.1_x86_64.deb
+		fi
 	fi
-fi
+}
 
+install_java
+install_node
+install_maven
+install_virtualbox
+install_vagrant
 
 source $HOME/.bashrc
 
--- documentation/scripts/ubuntu-16.04.sh
+++ documentation/scripts/ubuntu-16.04.sh
@@ -1,83 +1,94 @@
 #!/bin/bash
 
-###### Java #######
-if type -p java; then
-	echo found java executable in PATH
-	_java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-	echo found java executable in JAVA_HOME     
-	_java="$JAVA_HOME/bin/java"
-else
-	echo "no java"
-	exit 0
-fi
+function install_java {
+	if type -p java; then
+		echo found java executable in PATH
+		_java=java
+	elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+		echo found java executable in JAVA_HOME     
+		_java="$JAVA_HOME/bin/java"
+	else
+		echo "no java"
+		exit 0
+	fi
 
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ "$version" < "1.8" ]]; then
-	echo version is less than 1.8
-	exit 0
-    fi
-fi
+	if [[ "$_java" ]]; then
+	    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+	    echo version "$version"
+	    if [[ "$version" < "1.8" ]]; then
+		echo version is less than 1.8
+		exit 0
+	    fi
+	fi
+}
 
-###### Node #######
-curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
-sudo apt-get -y install nodejs
-NODEVER=$(node -v | cut -c2)
-if (( $NODEVER < 5 ))
-then
-	echo 'Node can not be installed'
-	exit 0
-fi
-
-###### Maven #######
-ISMAVEN=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $4}')
-
-if [ "$ISMAVEN" != "installed" ]
-then 
-	sudo apt-get -y update
-	sudo apt-get -y install maven
-else
-	MAVENVER=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $2}' | colrm 2)
-	if (( $MAVENVER < 3 ))
+function install_node {
+	curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
+	sudo apt-get -y install nodejs
+	NODEVER=$(node -v | cut -c2)
+	if (( $NODEVER < 5 ))
 	then
-		sudo apt-get -y remove maven
+		echo 'Node can not be installed'
+		exit 0
+	fi
+}
+
+function install_maven {
+	ISMAVEN=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $4}')
+
+	if [ "$ISMAVEN" != "installed" ]
+	then 
 		sudo apt-get -y update
 		sudo apt-get -y install maven
+	else
+		MAVENVER=$(dpkg -s maven | grep -e Version | head -n 1 | awk '{print $2}' | colrm 2)
+		if (( $MAVENVER < 3 ))
+		then
+			sudo apt-get -y remove maven
+			sudo apt-get -y update
+			sudo apt-get -y install maven
+		fi
 	fi
-fi
+}
 
-###### VirtualBox #######
-ISVIRTUALBOX=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $4}')
+function install_virtualbox {
+	ISVIRTUALBOX=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $4}')
 
-if [ "$ISVIRTUALBOX" != "installed" ]
-then 
-	sudo apt -y install virtualbox-qt 
-else
-	VIRTUALBOXVER=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $2}' | colrm 6)
-	if ( (( "$(echo $VIRTUALBOXVER | cut -c1)" <= 5 )) && (( "$(echo $VIRTUALBOXVER | cut -c3)" <= 0 )) &&  (( "$(echo $VIRTUALBOXVER | cut -c5)" < 4)) )
-	then
+	if [ "$ISVIRTUALBOX" != "installed" ]
+	then 
 		sudo apt -y install virtualbox-qt 
+	else
+		VIRTUALBOXVER=$(dpkg -s virtualbox | grep -e Version | head -n 1 | awk '{print $2}' | colrm 6)
+		if ( (( "$(echo $VIRTUALBOXVER | cut -c1)" <= 5 )) && (( "$(echo $VIRTUALBOXVER | cut -c3)" <= 0 )) &&  (( "$(echo $VIRTUALBOXVER | cut -c5)" < 4)) )
+		then
+			sudo apt -y install virtualbox-qt 
+		fi
 	fi
-fi
+}
 
-###### Vagrant #######
-ISVAGRANT=$(dpkg -s vagrant | grep -e Status | head -n 1 | awk '{print $4}')
-echo $ISVAGRANT
+function install_vagrant {
+	ISVAGRANT=$(dpkg -s vagrant | grep -e Status | head -n 1 | awk '{print $4}')
+	echo $ISVAGRANT
 
-if [ "$ISVAGRANT" != "installed" ]
-then 
-	wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
-	sudo dpkg -i vagrant_1.8.1_x86_64.deb
-else
-	VAGRANTVER=$(dpkg -s vagrant | grep -e Version | head -n 1  | awk '{print $2}' | cut -c3-5)
-	if ( (( "$(echo $VAGRANTVER | cut -c1)" <= 1 )) && (( "$(echo $VAGRANTVER | cut -c3)" < 8 )) )
-	then
+	if [ "$ISVAGRANT" != "installed" ]
+	then 
 		wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
 		sudo dpkg -i vagrant_1.8.1_x86_64.deb
+	else
+		VAGRANTVER=$(dpkg -s vagrant | grep -e Version | head -n 1  | awk '{print $2}' | cut -c3-5)
+		if ( (( "$(echo $VAGRANTVER | cut -c1)" <= 1 )) && (( "$(echo $VAGRANTVER | cut -c3)" < 8 )) )
+		then
+			wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+			sudo dpkg -i vagrant_1.8.1_x86_64.deb
+		fi
 	fi
-fi
+}
+
+install_java
+install_node
+install_maven
+install_virtualbox
+install_vagrant
 
 source $HOME/.bashrc
 
