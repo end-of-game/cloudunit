1732668863f3c004e5601e58041979e1750be77e
nicolas <zepouet@gmail.com>
2016-05-04 10:20:36 +0200

    Update jenkins2 scripts

--- cu-platform/jenkins2.sh
+++ cu-platform/jenkins2.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
-docker run  --name jenkins2 \
-            -d -p 9080:8080 -p 50000:50000 \
-            -v /home/vagrant/docker:/var/jenkins_home \
-            jenkinsci/jenkins:2.0
+export JK_MAJOR=2.1
+
+docker images | grep jenkinsci/jenkins
+if [ "$?" == "1" ]; then
+    docker pull jenkinsci/jenkins:$JK_MAJOR
+fi
 
 
+docker inspect -f {{.State.Running}} jenkins2
+
+# If jenkins is not running
+if [ "$?" == "1" ]; then
+
+    mkdir -p /home/vagrant/jenkins_home
+
+    docker run  --name jenkins2 \
+                -d -p 9080:8080 -p 50000:50000 \
+                -v /home/vagrant/jenkins_home:/var/jenkins_home \
+                jenkinsci/jenkins:$JK_MAJOR
+
+    if [ "$?" == "1" ]; then
+        sudo chown -R vagrant /home/vagrant/jenkins_home
+        docker start jenkins2
+    fi
+fi
+
+docker ps -a | grep jenkins2
+
+echo -e "\nACCESS TO JENKINS AT --> http://192.168.50.4:9080\n"
