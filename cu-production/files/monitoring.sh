6cc86a440ccfd018d967242e37de3be779914033
nicolas <zepouet@gmail.com>
2016-05-18 10:51:38 +0200

    Add certificats for Tomcat production CU

--- cu-production/files/monitoring.sh
+++ cu-production/files/monitoring.sh
@@ -12,7 +12,7 @@ else
 	if [ ! -f /home/admincu/cloudunit/monitoring_scripts/cloudunitmonitor.jar ]; then
 		wget https://github.com/Treeptik/cloudunit/releases/download/1.0/cloudunitmonitor.jar -O /home/admincu/cloudunit/monitoring_scripts/cloudunitmonitor.jar
 	fi
-	java -Xms128m -Xmx128m -jar /home/admincu/cloudunit/monitoring_scripts/cloudunitmonitor.jar $(docker inspect --format {{.NetworkSettings.IPAddress}}  cuplatform_mysql_1) $MYSQL_ROOT_PASSWORD $(docker inspect --format {{.NetworkSettings.IPAddress}} cuplatform_redis_1 https)
+	java -Xms128m -Xmx128m -jar /home/admincu/cloudunit/monitoring_scripts/cloudunitmonitor.jar $(docker inspect --format {{.NetworkSettings.IPAddress}}  cuproduction_mysql_1) $MYSQL_ROOT_PASSWORD $(docker inspect --format {{.NetworkSettings.IPAddress}} cuproduction_redis_1 https)
 
 	rm $LOCK_CM
 fi
