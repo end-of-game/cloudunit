40a95f6f732539dd4c4fcadeb51e4f848f8407d6
nicolas <zepouet@gmail.com>
2016-05-19 17:47:09 +0200

    add init function

--- cu-production/boot.sh
+++ cu-production/boot.sh
@@ -31,7 +31,6 @@ function provision_packages {
 function provision_docker {
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
   cp $CU_INSTALL_DIR/files/sources.list /etc/apt/sources.list
-  apt-get update
   apt-get install -y lxc-docker-1.6.2 1.6.2
   apt-mark hold lxc-docker
   apt-get install -y linux-image-extra-$(uname -r)
@@ -78,6 +77,7 @@ function start_cloudunit {
 
 # ------------------------------
 
+init
 create_admincu
 provision_packages
 clone_cloudunit
