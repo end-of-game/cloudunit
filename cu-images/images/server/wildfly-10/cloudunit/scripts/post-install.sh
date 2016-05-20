1c3d2c5d2d6f3a161b4bfee60684f12a2110ea4a
nicolas <zepouet@gmail.com>
2016-05-20 14:13:04 +0200

    New architecture for wildfly 10

--- cu-images/images/server/wildfly-10/cloudunit/scripts/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/jboss/wildfly/bin/add-user.sh admin admin --silent
-
-wget https://github.com/Treeptik/cloudunit/releases/download/1.0/cloudunitAgent-1.0-SNAPSHOT.jar -O /cloudunit/tools/cloudunitAgent-1.0-SNAPSHOT.jar
-
-
