d73679ab8c77379fd43a70dbe695463adb12bd7d
nicolas <zepouet@gmail.com>
2016-05-23 11:27:47 +0200

    Update doc server installation

--- cu-production/boot.sh
+++ cu-production/boot.sh
@@ -52,9 +52,8 @@ service docker start
 su -l $CU_USER -c "cd $CU_HOME/cu-services && ./build-services.sh"
 
 # COMPILE ROOT WAR FOR CLOUDUNIT
-cd $CU_HOME/cu-manager
-./compile-root-war.sh
-cp target/ROOT.war $CU_HOME/cu-production/tomcat
+cd $CU_HOME/cu-manager && ./compile-root-war.sh
+mkdir -p $CU_HOME/cu-production/tomcat && cp target/ROOT.war $CU_HOME/cu-production/tomcat
 chown -R $CU_USER:$CU_USER /home/$CU_USER/.docker
 
 # RESET ALL FOR FIRST START
