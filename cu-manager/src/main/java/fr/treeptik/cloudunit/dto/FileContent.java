a2c54052788c7cfd1a275a6af932e657e43ac16c
nicolas <zepouet@gmail.com>
2016-06-10 16:24:20 +0200

    Test RequestBody

---
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/dto/FileContent.java
@@ -0,0 +1,17 @@
+package fr.treeptik.cloudunit.dto;
+
+/**
+ * Created by nicolas on 10/06/2016.
+ */
+public class FileContent {
+
+    private String fileContent;
+
+    public String getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;
+    }
+}
