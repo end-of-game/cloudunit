93c3d4700dd16e956dce8ce88d47ac5e63b6fc44
nicolas <zepouet@gmail.com>
2016-06-15 15:20:57 +0200

    Add mock execution

--- cu-manager/src/main/java/fr/treeptik/cloudunit/config/SecurityConfiguration.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/config/SecurityConfiguration.java
@@ -142,6 +142,7 @@ public class SecurityConfiguration
             .antMatchers("/snapshot/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
             .antMatchers("/monitoring/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
             .antMatchers("/messages/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+            .antMatchers("/scripting/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
             .antMatchers("/gitlab/**").permitAll()
             .antMatchers("/nopublic/**").permitAll().and()
             .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
