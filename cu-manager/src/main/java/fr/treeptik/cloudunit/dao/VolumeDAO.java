ec9ce43ec8eb59ce91b54ddbfb5ff5712d3d77c0
Kan23 <romain.lelievre93@gmail.com>
2016-08-10 13:42:47 +0200

    Add Volume model, DAO and DTO

--- cu-manager/src/main/java/fr/treeptik/cloudunit/dao/VolumeDAO.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/dao/VolumeDAO.java
@@ -1,7 +1,25 @@
 package fr.treeptik.cloudunit.dao;
 
-/**
- * Created by stagiaire on 10/08/16.
- */
-public interface VolumeDAO {
+import fr.treeptik.cloudunit.model.Volume;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface VolumeDAO extends JpaRepository<Volume, Integer> {
+
+    @Query("Select v from Volume v where v.application.name=:name")
+    List<Volume> findByApplicationName(@Param("name") String name)
+            throws DataAccessException;
+
+    @Query("Select v from Volume v where v.id=:id")
+    Volume findById(@Param("id") int id)
+            throws DataAccessException;
+
+    @Query("select v from Volume v")
+    List<Volume> findAllVolumes()
+            throws DataAccessException;
 }
+
--- cu-manager/src/main/java/fr/treeptik/cloudunit/dto/VolumeRequest.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/dto/VolumeRequest.java
@@ -1,7 +1,37 @@
 package fr.treeptik.cloudunit.dto;
 
-/**
- * Created by stagiaire on 10/08/16.
- */
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VolumeRequest {
+
+    private Integer id;
+
+    private String name;
+
+    private String path;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
