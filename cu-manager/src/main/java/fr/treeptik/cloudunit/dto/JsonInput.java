7c35e55f2356ab24fd14699590c99470b00d8b42
Guillaume <g.martial@treeptik.fr>
2016-08-10 10:14:44 +0200

    FEAT-prevent null pointer on jvm options

--- cu-manager/src/main/java/fr/treeptik/cloudunit/dto/JsonInput.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/dto/JsonInput.java
@@ -23,213 +23,213 @@ import java.io.Serializable;
 /**
  * Created by nicolas on 31/07/2014.
  */
-public class JsonInput
-    implements Serializable {
+public class JsonInput implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String applicationName;
+	private String applicationName;
 
-    private String jvmMemory;
+	private String jvmMemory;
 
-    private String jvmOptions;
+	private String jvmOptions;
 
-    private String serverName;
+	private String serverName;
 
-    private String imageName;
+	private String imageName;
 
-    private String login;
+	private String login;
 
-    private String location;
+	private String location;
 
-    private String alias;
+	private String alias;
 
-    private String jvmRelease;
+	private String jvmRelease;
 
-    private String tag;
+	private String tag;
 
-    private String description;
+	private String description;
 
-    private String clientSource;
+	private String clientSource;
 
-    private String moduleName;
+	private String moduleName;
 
-    private String portToOpen;
+	private String portToOpen;
 
-    private String portNature;
+	private String portNature;
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
 
-    public String getJvmRelease() {
-        return jvmRelease;
-    }
+	public String getJvmRelease() {
+		return jvmRelease;
+	}
 
-    public void setJvmRelease(String jvmRelease) {
-        this.jvmRelease = jvmRelease;
-    }
+	public void setJvmRelease(String jvmRelease) {
+		this.jvmRelease = jvmRelease;
+	}
 
-    @Override
-    public String toString() {
-        return "JsonInput{" + "applicationName='" + applicationName + '\''
-            + ", jvmMemory='" + jvmMemory + '\'' + ", jvmOptions='"
-            + jvmOptions + '\'' + ", serverName='" + serverName + '\''
-            + ", imageName='" + imageName + '\'' + ", login='" + login
-            + '\'' + ", location='" + location + '\'' + ", moduleName='"
-            + moduleName + '\'' + ", alias='" + alias + '\'' + '}';
-    }
+	@Override
+	public String toString() {
+		return "JsonInput{" + "applicationName='" + applicationName + '\'' + ", jvmMemory='" + jvmMemory + '\''
+				+ ", jvmOptions='" + jvmOptions + '\'' + ", serverName='" + serverName + '\'' + ", imageName='"
+				+ imageName + '\'' + ", login='" + login + '\'' + ", location='" + location + '\'' + ", moduleName='"
+				+ moduleName + '\'' + ", alias='" + alias + '\'' + '}';
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    public String getApplicationName() {
-        return applicationName;
-    }
+	public String getApplicationName() {
+		return applicationName;
+	}
 
-    public void setApplicationName(String applicationName) {
-        this.applicationName = applicationName;
-    }
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
 
-    public String getJvmMemory() {
-        return jvmMemory;
-    }
+	public String getJvmMemory() {
+		return jvmMemory;
+	}
 
-    public void setJvmMemory(String jvmMemory) {
-        this.jvmMemory = jvmMemory;
-    }
+	public void setJvmMemory(String jvmMemory) {
+		this.jvmMemory = jvmMemory;
+	}
 
-    public String getJvmOptions() {
-        return jvmOptions.replace("\\n", " ");
-    }
+	public String getJvmOptions() {
+		if (jvmOptions != null) {
+			return jvmOptions.replace("\\n", " ");
+		} else
+			return jvmOptions;
+	}
 
-    public void setJvmOptions(String jvmOptions) {
-        this.jvmOptions = jvmOptions;
-    }
+	public void setJvmOptions(String jvmOptions) {
+		this.jvmOptions = jvmOptions;
+	}
 
-    public String getServerName() {
-        return serverName;
-    }
+	public String getServerName() {
+		return serverName;
+	}
 
-    public void setServerName(String serverName) {
-        this.serverName = serverName;
-    }
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
 
-    public String getImageName() {
-        return imageName;
-    }
+	public String getImageName() {
+		return imageName;
+	}
 
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
 
-    public String getModuleName() {
-        return moduleName;
-    }
+	public String getModuleName() {
+		return moduleName;
+	}
 
-    public void setModuleName(String moduleName) {
-        this.moduleName = moduleName;
-    }
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public String getAlias() {
-        return alias;
-    }
+	public String getAlias() {
+		return alias;
+	}
 
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
 
-    public String getTag() {
-        return tag;
-    }
+	public String getTag() {
+		return tag;
+	}
 
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getClientSource() {
-        return clientSource;
-    }
+	public String getClientSource() {
+		return clientSource;
+	}
 
-    public void setClientSource(String clientSource) {
-        this.clientSource = clientSource;
-    }
+	public void setClientSource(String clientSource) {
+		this.clientSource = clientSource;
+	}
 
-    public String getPortToOpen() {
-        return portToOpen;
-    }
+	public String getPortToOpen() {
+		return portToOpen;
+	}
 
-    public void setPortToOpen(String portToOpen) {
-        this.portToOpen = portToOpen;
-    }
+	public void setPortToOpen(String portToOpen) {
+		this.portToOpen = portToOpen;
+	}
 
-    public String getPortNature() {
-        return this.portNature;
-    }
+	public String getPortNature() {
+		return this.portNature;
+	}
 
-    public void setPortNature(String portNature) {
-        this.portNature = portNature;
-    }
+	public void setPortNature(String portNature) {
+		this.portNature = portNature;
+	}
 
-    // VALIDATIONS
+	// VALIDATIONS
 
-    public void validateCreateApp() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-        CheckUtils.validateInput(serverName, "check.server.name");
-    }
+	public void validateCreateApp() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+		CheckUtils.validateInput(serverName, "check.server.name");
+	}
 
-    public void validateStartApp() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-    }
+	public void validateStartApp() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+	}
 
-    public void validateStopApp() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-    }
+	public void validateStopApp() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+	}
 
-    public void validateRemoveApp() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-    }
+	public void validateRemoveApp() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+	}
 
-    public void validateAddModule() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-        CheckUtils.validateInput(imageName, "check.image.name");
-    }
+	public void validateAddModule() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+		CheckUtils.validateInput(imageName, "check.image.name");
+	}
 
-    public void validateRemoveModule() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-        CheckUtils.validateInputNotEmpty(moduleName, "check.module.name");
-    }
+	public void validateRemoveModule() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+		CheckUtils.validateInputNotEmpty(moduleName, "check.module.name");
+	}
 
-    public void validateDetail() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-    }
+	public void validateDetail() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+	}
 
-    public void validateCreateSnapshot() throws CheckException {
-        CheckUtils.validateSyntaxInput(tag, "check.snapshot.name");
-    }
+	public void validateCreateSnapshot() throws CheckException {
+		CheckUtils.validateSyntaxInput(tag, "check.snapshot.name");
+	}
 
-    public void validateClone() throws CheckException {
-        CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
-    }
+	public void validateClone() throws CheckException {
+		CheckUtils.validateSyntaxInput(applicationName, "check.app.name");
+	}
 }
--- cu-manager/src/main/java/fr/treeptik/cloudunit/utils/CheckUtils.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/utils/CheckUtils.java
@@ -31,167 +31,154 @@ import java.util.Locale;
 @SuppressWarnings("ALL")
 public class CheckUtils {
 
-    private static final List<String> listJvmMemoriesAllowed = Arrays.asList(JvmMemory.SIZE_512.getSize(),
-            JvmMemory.SIZE_1024.getSize(),
-            JvmMemory.SIZE_2048.getSize(),
-            JvmMemory.SIZE_3072.getSize(),
-            JvmMemory.SIZE_4096.getSize());
+	private static final List<String> listJvmMemoriesAllowed = Arrays.asList(JvmMemory.SIZE_512.getSize(),
+			JvmMemory.SIZE_1024.getSize(), JvmMemory.SIZE_2048.getSize(), JvmMemory.SIZE_3072.getSize(),
+			JvmMemory.SIZE_4096.getSize());
 
-    private static final List<String> listJvmReleaseAllowed = Arrays.asList(
-            JavaRelease.Java7.getVersion(), JavaRelease.Java8.getVersion());
+	private static final List<String> listJvmReleaseAllowed = Arrays.asList(JavaRelease.Java7.getVersion(),
+			JavaRelease.Java8.getVersion());
 
-    private static MessageSource messageSource = (MessageSource) StaticSpringApplicationContext.getBean("messageSource");
+	private static MessageSource messageSource = (MessageSource) StaticSpringApplicationContext
+			.getBean("messageSource");
 
-    /**
-     * Valid not empty input
-     *
-     * @param field
-     * @param message
-     * @throws CheckException
-     */
-    public static void validateInputNotEmpty(String field, String message)
-            throws CheckException {
-        validateInputSizeMax(field, message, Integer.MAX_VALUE);
-    }
+	/**
+	 * Valid not empty input
+	 *
+	 * @param field
+	 * @param message
+	 * @throws CheckException
+	 */
+	public static void validateInputNotEmpty(String field, String message) throws CheckException {
+		validateInputSizeMax(field, message, Integer.MAX_VALUE);
+	}
 
-    /**
-     * Valid classic input.
-     *
-     * @param field
-     * @param message
-     * @throws CheckException
-     */
-    public static void validateInput(String field, String message)
-            throws CheckException {
-        validateInputSizeMax(field, message, 64);
-    }
+	/**
+	 * Valid classic input.
+	 *
+	 * @param field
+	 * @param message
+	 * @throws CheckException
+	 */
+	public static void validateInput(String field, String message) throws CheckException {
+		validateInputSizeMax(field, message, 64);
+	}
 
-    /**
-     * Valid a field input with a max size.
-     *
-     * @param field
-     * @param message
-     * @param size
-     * @throws CheckException
-     */
-    public static void validateInputSizeMax(String field, String message, int size)
-            throws CheckException {
-        if (field == null
-                || field.trim().length() == 0
-                /*|| "undefined".equals(field)*/
-                || field.length() > size) {
-            String messageTranslated = messageSource.getMessage(message, null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + field);
-        }
-    }
+	/**
+	 * Valid a field input with a max size.
+	 *
+	 * @param field
+	 * @param message
+	 * @param size
+	 * @throws CheckException
+	 */
+	public static void validateInputSizeMax(String field, String message, int size) throws CheckException {
+		if (field == null || field.trim().length() == 0
+		/* || "undefined".equals(field) */
+				|| field.length() > size) {
+			String messageTranslated = messageSource.getMessage(message, null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + field);
+		}
+	}
 
-    /**
-     * To validate if port is free or occupied.
-     *
-     * @param port
-     * @param application
-     * @throws CheckException
-     */
-    public static void isPortFree(String port, Application application)
-            throws CheckException {
-        Long numberOfThisPort = application.getPortsToOpen().stream().filter(t -> t.getPort().equals(Integer.parseInt(port))).count();
-        if (numberOfThisPort != 0) {
-            String messageTranslated = messageSource.getMessage("port.already.used", null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + port);
-        }
-    }
+	/**
+	 * To validate if port is free or occupied.
+	 *
+	 * @param port
+	 * @param application
+	 * @throws CheckException
+	 */
+	public static void isPortFree(String port, Application application) throws CheckException {
+		Long numberOfThisPort = application.getPortsToOpen().stream()
+				.filter(t -> t.getPort().equals(Integer.parseInt(port))).count();
+		if (numberOfThisPort != 0) {
+			String messageTranslated = messageSource.getMessage("port.already.used", null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + port);
+		}
+	}
 
-    /**
-     * Validate the syntax port.
-     *
-     * @param port
-     * @param application
-     * @throws CheckException
-     */
-    public static void validateOpenPort(String port, Application application)
-            throws CheckException {
+	/**
+	 * Validate the syntax port.
+	 *
+	 * @param port
+	 * @param application
+	 * @throws CheckException
+	 */
+	public static void validateOpenPort(String port, Application application) throws CheckException {
 
-        try {
-            Integer.parseInt(port);
-        } catch (NumberFormatException e) {
-            String messageTranslated = messageSource.getMessage("port.format", null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + port);
-        }
+		try {
+			Integer.parseInt(port);
+		} catch (NumberFormatException e) {
+			String messageTranslated = messageSource.getMessage("port.format", null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + port);
+		}
 
-        if (Integer.parseInt(port) <=0 ) {
-            String messageTranslated = messageSource.getMessage("port.format", null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + port);
-        }
-    }
+		if (Integer.parseInt(port) <= 0) {
+			String messageTranslated = messageSource.getMessage("port.format", null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + port);
+		}
+	}
 
-    /**
-     * To validate the nature associated to the port feature
-     *
-     * @param nature
-     * @param application
-     * @throws CheckException
-     */
-    public static void validateNatureForOpenPortFeature(String nature, Application application)
-            throws CheckException {
+	/**
+	 * To validate the nature associated to the port feature
+	 *
+	 * @param nature
+	 * @param application
+	 * @throws CheckException
+	 */
+	public static void validateNatureForOpenPortFeature(String nature, Application application) throws CheckException {
 
-        if (nature == null || nature.trim().length()==0) {
-            String messageTranslated = messageSource.getMessage("port.nature", null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + nature);
-        }
+		if (nature == null || nature.trim().length() == 0) {
+			String messageTranslated = messageSource.getMessage("port.nature", null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + nature);
+		}
 
-        if(!nature.equalsIgnoreCase("web") && !nature.equalsIgnoreCase("other")) {
-            String messageTranslated = messageSource.getMessage("port.nature", null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated + " : " + nature);
-        }
-    }
+		if (!nature.equalsIgnoreCase("web") && !nature.equalsIgnoreCase("other")) {
+			String messageTranslated = messageSource.getMessage("port.nature", null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated + " : " + nature);
+		}
+	}
 
+	/**
+	 * Valid Classic + Syntax input
+	 *
+	 * @param field
+	 * @param message
+	 * @throws CheckException
+	 */
+	public static void validateSyntaxInput(String field, String message) throws CheckException {
 
-    /**
-     * Valid Classic + Syntax input
-     *
-     * @param field
-     * @param message
-     * @throws CheckException
-     */
-    public static void validateSyntaxInput(String field, String message)
-            throws CheckException {
+		if (field == null || field.trim().length() == 0
+		// || "undefined".equals(field)
+				|| field.length() > 25)
+		// || !StringUtils.isAlphanumeric(field))
+		{
 
-        if (field == null
-                || field.trim().length() == 0
-                //|| "undefined".equals(field)
-                || field.length() > 25)
-                //|| !StringUtils.isAlphanumeric(field))
-        {
+			String messageTranslated = messageSource.getMessage(message, null, Locale.ENGLISH);
+			throw new CheckException(messageTranslated);
+		}
+	}
 
-            String messageTranslated = messageSource.getMessage(message, null, Locale.ENGLISH);
-            throw new CheckException(messageTranslated);
-        }
-    }
+	/**
+	 * Verify the input for the jvm options
+	 *
+	 * @param jvmOpts
+	 * @param jvmMemory
+	 * @param jvmRelease
+	 * @throws CheckException
+	 */
+	public static void checkJavaOpts(String jvmOpts, String jvmMemory, String jvmRelease) throws CheckException {
 
-    /**
-     * Verify the input for the jvm options
-     *
-     * @param jvmOpts
-     * @param jvmMemory
-     * @param jvmRelease
-     * @throws CheckException
-     */
-    public static void checkJavaOpts(String jvmOpts, String jvmMemory, String jvmRelease)
-            throws CheckException {
+		if (jvmOpts != null && jvmOpts.toLowerCase().contains("xms") || jvmOpts.toLowerCase().contains("xmx")) {
+			throw new CheckException("You are not allowed to change memory with java opts");
+		}
 
-        if (jvmOpts.toLowerCase().contains("xms")
-                || jvmOpts.toLowerCase().contains("xmx")) {
-            throw new CheckException("You are not allowed to change memory with java opts");
-        }
+		if (!listJvmMemoriesAllowed.contains(jvmMemory)) {
+			throw new CheckException("You are not allowed to set this jvm memory size : [" + jvmMemory + "]");
+		}
 
-        if (!listJvmMemoriesAllowed.contains(jvmMemory)) {
-            throw new CheckException("You are not allowed to set this jvm memory size : [" + jvmMemory + "]");
-        }
-
-        if (!listJvmReleaseAllowed.contains(jvmRelease)) {
-            throw new CheckException("You are not allowed to set this jvm release : [" + jvmRelease + "]");
-        }
-    }
+		if (!listJvmReleaseAllowed.contains(jvmRelease)) {
+			throw new CheckException("You are not allowed to set this jvm release : [" + jvmRelease + "]");
+		}
+	}
 
 }
-
