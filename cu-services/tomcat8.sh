74ad9360db4d616a1dbdf1a43663bc7b3189dc79
nicolas <zepouet@gmail.com>
2016-08-12 15:35:31 +0200

    Tomcat 85 + Tomcat 9 M9

---
+++ cu-services/tomcat8.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build --rm --no-cache -t cloudunit/base-14.04 images/base-14.04
+docker build --rm --no-cache -t cloudunit/tomcat-8 images/servers/appconf/tomcat-8
---
+++ cu-services/tomcat9.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build --rm --no-cache -t cloudunit/base-14.04 images/base-14.04
+docker build --rm --no-cache -t cloudunit/tomcat-9 images/servers/appconf/tomcat-9
