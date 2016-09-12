10be5661dae898c9f9a805c0b9605ce7f13ddc81
sebastienmusso <musso.sebastien@gmail.com>
2016-09-12 11:25:09 +0200

    Refactorize building of tomcat and wildfly image server

--- cu-services/tomcat8.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --rm --no-cache -t cloudunit/base-14.04 images/base-14.04
-docker build --rm --no-cache -t cloudunit/tomcat-8 images/servers/tomcat-8
--- cu-services/wildfly10.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --rm --no-cache -t cloudunit/base-14.04 images/base-14.04
-docker build --rm -t cloudunit/wildfly-10 images/servers/wildfly-10
-
-
