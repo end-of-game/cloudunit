e4f8f8300f102f409625236049469e84ac9de220
Guillaume <g.martial@treeptik.fr>
2016-09-15 11:25:07 +0200

    FEAT-add expose port module command in CLI

--- cu-cli/src/main/java/fr/treeptik/cloudunit/cli/commands/ModuleCommands.java
+++ cu-cli/src/main/java/fr/treeptik/cloudunit/cli/commands/ModuleCommands.java
@@ -28,24 +28,25 @@ public class ModuleCommands implements CommandMarker {
     private ModuleUtils moduleUtils;
 
     @CliCommand(value = "add-module", help = "Add a new module to the current application")
-    public String addModule(
-            @CliOption(key = {"name"}, mandatory = true, help = "Module type " +
-                    "\n MYSQL 5.5 : -name mysql-5-5 " +
-                    "\n MYSQL 5.6 : -name mysql-5-6 " +
-                    "\n MYSQL 5.7 : -name mysql-5-7 " +
-                    "\n POSTGRES 9.3 : -name postgresql-9-3 " +
-                    "\n POSTGRES 9.4 : -name postgresql-9-4 " +
-                    "\n POSTGRES 9.5 : -name postgresql-9-5 " +
-                    "\n REDIS 3.0 : -name redis-3-0 " +
-                    "\nMongo 2.6 : -name mongo-2-6") String moduleName) {
+    public String addModule(@CliOption(key = { "name" }, mandatory = true, help = "Module type "
+            + "\n MYSQL 5.5 : -name mysql-5-5 " + "\n MYSQL 5.6 : -name mysql-5-6 " + "\n MYSQL 5.7 : -name mysql-5-7 "
+            + "\n POSTGRES 9.3 : -name postgresql-9-3 " + "\n POSTGRES 9.4 : -name postgresql-9-4 "
+            + "\n POSTGRES 9.5 : -name postgresql-9-5 " + "\n REDIS 3.0 : -name redis-3-0 "
+            + "\nMongo 2.6 : -name mongo-2-6") String moduleName) {
 
         return moduleUtils.addModule(moduleName, null);
     }
 
     @CliCommand(value = "rm-module", help = "Remove a module from the current application")
-    public String removeModule(
-            @CliOption(key = {"name"}, mandatory = true, help = "Name of the module. Use show-modules command to get all modules of this application") String moduleName) {
+    public String removeModule(@CliOption(key = {
+            "name" }, mandatory = true, help = "Name of the module. Use show-modules command to get all modules of this application") String moduleName) {
         return moduleUtils.removeModule(moduleName);
+    }
+
+    @CliCommand(value = "expose-port", help = "Expose the default module port")
+    public String exposePort(@CliOption(key = {
+            "name" }, mandatory = true, help = "Name of the module. Use show-modules command to get all modules of this application") String moduleName) {
+        return null;
     }
 
     @CliCommand(value = "display-modules", help = "Display informations about all modules of the current application")
