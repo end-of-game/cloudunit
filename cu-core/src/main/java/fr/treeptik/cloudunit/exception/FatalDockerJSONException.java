cbcc67a33d9a8a5a35b6514ca6c8d789c0b8c14c
nicolas <zepouet@gmail.com>
2016-09-29 18:17:10 +0200

    Fix : Correct dependency and move common exceptions between cu-api and cu-core (duplicate exception into same package)

--- cu-core/src/main/java/fr/treeptik/cloudunit/exception/FatalDockerJSONException.java
+++ cu-core/src/main/java/fr/treeptik/cloudunit/exception/FatalDockerJSONException.java
@@ -17,13 +17,13 @@
 
 package fr.treeptik.cloudunit.exception;
 
-public class WarningDockerJSONException
-    extends DockerJSONException {
+public class FatalDockerJSONException
+        extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public WarningDockerJSONException(String message) {
-        super(message);
+    public FatalDockerJSONException(String message, Throwable e) {
+        super(message, e);
     }
 
 }
