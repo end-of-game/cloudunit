ca1927bccb671bda980d109444698f8146dc58f0
nicolas <zepouet@gmail.com>
2016-09-30 16:14:52 +0200

    FIX : Correct dnsdock rules policy

--- cu-compose/reset.sh
+++ cu-compose/reset.sh
@@ -14,8 +14,21 @@
 #!/usr/bin/env bash
 
 docker rm -vf $(docker ps -aq)
-docker-compose up -d
+docker-compose up -d dnsdock
+sleep 10
+docker-compose up -d mysqldata
+docker-compose up -d mysql
+sleep 10
+docker-compose up -d cadvisor
+docker-compose up -d hipache
+docker-compose up -d redis
+docker-compose up -d gitlab
+docker-compose up -d jenkins
+docker-compose up -d tomcat
+sleep 60
+docker-compose up -d nginx
 docker-compose logs -f
+
 
 
 
