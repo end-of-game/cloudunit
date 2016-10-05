f507c0760ce239d730ee0f14b11c1f8849030033
nicolas <zepouet@gmail.com>
2016-10-05 16:24:31 +0200

    Merge manual

--- cu-services/images/servers/fatjar/scripts/check-running.sh
+++ cu-services/images/servers/fatjar/scripts/check-running.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RETURN=0
+ps -ef | grep java
+RETURN=$?
 
 # The echo is used by docker-exec to know if results are right
 # do not remove it
--- cu-services/images/servers/fatjar/scripts/deploy.sh
+++ cu-services/images/servers/fatjar/scripts/deploy.sh
@@ -4,12 +4,8 @@ set -x
 
 CU_USER=$1
 CU_PASSWORD=$2
+CU_FILE=$3
+CU_CONTEXT_PATH=$4
 
-FILETODEPLOY=`ls $CU_TMP`
-
-rm -rf $CU_SOFTWARE/deployments/*
-
-mv $CU_TMP/$FILETODEPLOY $CU_SOFTWARE/deployments
-
-$JAVA_HOME/bin/java -jar $CU_SOFTWARE/deployments/$FILETODEPLOY > $CU_LOGS/system.out &
+mv -f $CU_TMP/$CU_FILE /opt/cloudunit/fatjar/boot.jar
 
