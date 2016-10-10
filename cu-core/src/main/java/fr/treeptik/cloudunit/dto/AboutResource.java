5d05fcca1a97c8df0f23dafd22b5ededf5f1264a
William Bartlett <wbartlett@treeptik.fr>
2016-10-10 16:54:14 +0200

    FEAT- add resource for getting version information from manager API

---
+++ cu-core/src/main/java/fr/treeptik/cloudunit/dto/AboutResource.java
@@ -0,0 +1,25 @@
+package fr.treeptik.cloudunit.dto;
+
+/**
+ * Resource for reporting version information.
+ * 
+ * @author William Bartlett
+ */
+public class AboutResource {
+    private String version;
+    private String timestamp;
+    
+    public AboutResource(String version, String timestamp) {
+        super();
+        this.version = version;
+        this.timestamp = timestamp;
+    }
+    
+    public String getVersion() {
+        return version;
+    }
+    
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
