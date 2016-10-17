ba1b1419f10d524d7202386ce996a88e9d4d5c58
smusso-treeptik <s.musso@treeptik.fr>
2016-10-17 08:54:47 +0200

    Add dashboard import automation on elk startup

--- cu-elk/kibana/docker-entrypoint.sh
+++ cu-elk/kibana/docker-entrypoint.sh
@@ -15,4 +15,19 @@ if [ "$1" = 'kibana' ]; then
 	set -- gosu kibana tini -- "$@"
 fi
 
+echo 'Waiting for elasticsearch Up and Running'
+until $(curl --output /dev/null --silent --head --fail http://elasticsearch:9200); do
+  sleep 2s
+done
+  sleep 1s
+
+if $(curl --output /dev/null --silent --head --fail http://elasticsearch:9200/.kibana)
+then
+  echo "Kibana index already exist"
+else
+	echo "Load Kibana index"
+	elasticdump --input=file/kibana-index-mapping.json --output=http://elasticsearch:9200/.kibana --type=mapping
+	elasticdump --input=file/kibana-index.json --output=http://elasticsearch:9200/.kibana --type=data
+fi
+
 exec "$@"
