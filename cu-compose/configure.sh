cadea8ba8aff15b8a4ab50f651de8b4b9b77d039
nicolas <zepouet@gmail.com>
2016-10-18 10:45:50 +0200

    FEAT : Check env var during installation

--- cu-compose/configure.sh
+++ cu-compose/configure.sh
@@ -2,11 +2,19 @@
 
 source /etc/environment
 
+[ -z "$CU_PORTAL_URL" ] && echo "Need to set CU_PORTAL_URL in /etc/environment" && exit 1;
+[ -z "$CU_MANAGER_URL" ] && echo "Need to set CU_MANAGER_URL in /etc/environment" && exit 1;
+[ -z "$CU_GITLAB_URL" ] && echo "Need to set CU_GITLAB_URL in /etc/environment" && exit 1;
+[ -z "$CU_JENKINS_URL" ] && echo "Need to set CU_JENKINS_URL in /etc/environment" && exit 1;
+[ -z "$CU_KIBANA_URL" ] && echo "Need to set CU_KIBANA_URL in /etc/environment" && exit 1;
+
+echo ""
 echo "CU_PORTAL_URL="$CU_PORTAL_URL
 echo "CU_MANAGER_URL="$CU_MANAGER_URL
 echo "CU_GITLAB_URL="$CU_GITLAB_URL
 echo "CU_JENKINS_URL="$CU_JENKINS_URL
 echo "CU_KIBANA_URL="$CU_KIBANA_URL
+echo ""
 
 cp -f nginx/sites-enabled/cloudunit.conf.template nginx/sites-enabled/cloudunit.conf
 sed -i "s/CU_PORTAL_URL/$CU_PORTAL_URL/g" nginx/sites-enabled/cloudunit.conf
@@ -14,6 +22,7 @@ sed -i "s/CU_MANAGER_URL/$CU_MANAGER_URL/g" nginx/sites-enabled/cloudunit.conf
 sed -i "s/CU_JENKINS_URL/$CU_JENKINS_URL/g" nginx/sites-enabled/cloudunit.conf
 sed -i "s/CU_GITLAB_URL/$CU_GITLAB_URL/g" nginx/sites-enabled/cloudunit.conf
 sed -i "s/CU_KIBANA_URL/$CU_KIBANA_URL/g" nginx/sites-enabled/cloudunit.conf
+echo "File 'nginx/sites-enabled/cloudunit.conf' configured"
 
 cp -f nginx/www/portal/index.html.template nginx/www/portal/index.html
 sed -i "s/CU_PORTAL_URL/$CU_PORTAL_URL/g" nginx/www/portal/index.html
@@ -21,4 +30,5 @@ sed -i "s/CU_MANAGER_URL/$CU_MANAGER_URL/g" nginx/www/portal/index.html
 sed -i "s/CU_JENKINS_URL/$CU_JENKINS_URL/g" nginx/www/portal/index.html
 sed -i "s/CU_GITLAB_URL/$CU_GITLAB_URL/g" nginx/www/portal/index.html
 sed -i "s/CU_KIBANA_URL/$CU_KIBANA_URL/g" nginx/www/portal/index.html
-
+echo "File 'nginx/www/portal/index.html' configured"
+echo ""
