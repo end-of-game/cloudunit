9e2591d3109a819efebb9bf1e97d9db1b29b75be
nicolas <zepouet@gmail.com>
2016-11-16 11:30:48 +0100

    FIX : update cron script

--- cu-production/files/cron.sh
+++ cu-production/files/cron.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#echo `date` >> ~/cron.log
-#cd /home/admincu/cloudunit/cu-compose && /home/admincu/cloudunit/cu-compose/start-with-elk.sh
+STATUS=`docker inspect -f {{.State.Running}} cuplatform_tomcat_1`
+if [ "$STATUS"=="false" ]; then
+ cd /home/admincu/cloudunit/cu-compose && /home/admincu/cloudunit/cu-compose/start-with-elk.sh
+fi
