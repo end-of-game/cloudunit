63823effc0df29af7dd404726eebcd67e92874fd
nicolas <zepouet@gmail.com>
2016-11-16 16:35:08 +0100

    FIX : AUFS corruption after brutal electric reboot of server. Extrem usecase

--- cu-production/files/cron.sh
+++ cu-production/files/cron.sh
@@ -2,5 +2,10 @@
 
 STATUS=`docker inspect -f {{.State.Running}} cuplatform_tomcat_1`
 if [ "$STATUS"=="false" ]; then
- cd /home/admincu/cloudunit/cu-compose && /home/admincu/cloudunit/cu-compose/start-with-elk.sh
+    cd /home/admincu/cloudunit/cu-compose
+    /home/admincu/cloudunit/cu-compose/start-with-elk.sh
+    docker-compose kill hipache
+    docker-compose rm -f hipache
+    docker-compose up -d hipache
 fi
+
