4d154ee7b5ff984f35283381cf56e7e31e21e31b
nicolas <zepouet@gmail.com>
2016-11-17 10:37:59 +0100

    FEAT : add kibana as first class citizen for prod env

--- cu-compose/nginx/docker-entrypoint.sh
+++ cu-compose/nginx/docker-entrypoint.sh
@@ -14,7 +14,7 @@ function waitFor ( ) {
 waitFor Tomcat  http://cuplatform_tomcat_1.manager.cloud.unit:8080
 waitFor Jenkins http://cuplatform_jenkins_1.jenkins.cloud.unit:8080
 waitFor Gitlab  http://cuplatform_gitlab_1.gitlab-ce.cloud.unit
-waitFor Kibana  http://cuplatform_kibana_1.kibana.cloud.unit:5601
+waitFor Kibana  http://cuplatform_kibana_1.elk-kibana.cloud.unit:5601
 
 echo "Nginx is started"
 exec nginx -g "daemon off;"
