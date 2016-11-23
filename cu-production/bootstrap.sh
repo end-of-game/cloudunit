df8a66b88c054431598704d4147ed4d692036ef4
nicolas <zepouet@gmail.com>
2016-11-23 14:42:53 +0100

    Update installation procedure

--- cu-production/bootstrap.sh
+++ cu-production/bootstrap.sh
@@ -65,8 +65,8 @@ cp $CU_INSTALL_DIR/files/docker-logrotate /etc/logrotate.d/
 
 # Install cron restart
 mkdir -p /home/admincu/.cloudunit
-touch "*/3 * * * * admincu /home/admincu/.cloudunit/cron.sh" >> /etc/crontab
 cp $CU_INSTALL_DIR/files/cron.sh /home/admincu/.cloudunit/cron.sh
+touch "*/3 * * * * admincu /home/admincu/.cloudunit/cron.sh" >> /etc/crontab
 
 chmod +x /home/admincu/.cloudunit/cron.sh
 chown -R admincu /home/admincu/
