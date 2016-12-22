62813b5e9e412479cb92a17ea697c2797b4c1582
sebastienmusso <musso.sebastien@gmail.com>
2016-12-22 16:07:20 +0100

    Disable elasticsearch and kibana external port publishing

--- cu-compose/cu-docker-compose.sh
+++ cu-compose/cu-docker-compose.sh
@@ -45,9 +45,17 @@ function reset {
             exit 1
         fi
     fi
-    docker rm -vf $(docker ps -aq --filter "label=origin=application")
+    
+
+    for container in $(docker ps -aq --format '{{.Names}}' --filter "label=origin=application"); do
+      echo "Delete applicative container "$container
+      docker rm -f $container
+      docker volume rm $container
+      docker volume ls -qf dangling=true | xargs -r docker volume rm
+    done
     docker-compose  -f docker-compose.elk.yml -f docker-compose.selenium.yml -f docker-compose.yml kill
     docker-compose  -f docker-compose.elk.yml -f docker-compose.selenium.yml -f docker-compose.yml rm -f
+    docker volume rm cucompose_elasticsearch-data
     docker volume rm cucompose_gitlab-logs
     docker volume rm cucompose_mysqldata
     docker volume rm cucompose_redis-data
