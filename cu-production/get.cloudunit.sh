b01f9cf18b0d246f7dc5e44d3a9f401b283a74da
sebastienmusso <musso.sebastien@gmail.com>
2016-12-23 10:05:18 +0100

    Update get.cloudunit.sh

--- cu-production/get.cloudunit.sh
+++ cu-production/get.cloudunit.sh
@@ -59,5 +59,4 @@ check_prerequisite() {
 
 check_prerequisite
 
-curl -sSL https://raw.githubusercontent.com/Treeptik/cloudunit/dev/cu-production/bootstrap.sh > bootstrap.sh 
-sh bootstrap.sh
+curl -sSL https://raw.githubusercontent.com/Treeptik/cloudunit/dev/cu-production/bootstrap.sh > bootstrap.sh
