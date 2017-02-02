fc49639b04265dc59c2aa4e37c9d0daeb3d06c85
nicolas <zepouet@gmail.com>
2017-02-02 15:09:21 +0100

    Fix : add default value for homepage portal

--- cu-manager/src/main/java/fr/treeptik/cloudunit/controller/HomepageController.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/controller/HomepageController.java
@@ -18,19 +18,19 @@ public class HomepageController {
     @Value("#{environment.CU_JENKINS_DOMAIN ?: 'jenkins.cloudunit.dev'}")
     private String jenkins;
 
-    @Value("#{environment.CU_GITLAB_DOMAIN} ?: 'gitlab.cloudunit.dev'")
+    @Value("#{environment.CU_GITLAB_DOMAIN ?: 'gitlab.cloudunit.dev'}")
     private String gitlab;
 
-    @Value("#{environment.CU_NEXUS_DOMAIN} ?: 'nexus.cloudunit.dev'")
+    @Value("#{environment.CU_NEXUS_DOMAIN ?: 'nexus.cloudunit.dev'}")
     private String nexus;
 
     @Value("#{environment.CU_KIBANA_DOMAIN ?: 'kibana.cloudunit.dev'}")
     private String kibana;
 
-    @Value("#{environment.CU_SONAR_DOMAIN} ?: 'sonar.cloudunit.dev'")
+    @Value("#{environment.CU_SONAR_DOMAIN ?: 'sonar.cloudunit.dev'}")
     private String sonar;
 
-    @Value("#{environment.CU_LETSCHAT_DOMAIN} ?: 'letschat.cloudunit.dev'")
+    @Value("#{environment.CU_LETSCHAT_DOMAIN ?: 'letschat.cloudunit.dev'}")
     private String letschat;
 
     @RequestMapping(value = "/friends", method = RequestMethod.GET)
