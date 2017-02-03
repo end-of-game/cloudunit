78a68fde9f092930aa6ec1b1dfa5b5ff68be0c70
GuillaumeUnice <guillaume.unice@gmail.com>
2017-02-03 09:47:05 +0100

    Correct bug Option Group broken

--- cu-manager-ui/app/scripts/components/dashboard/dashboard.createApplication.js
+++ cu-manager-ui/app/scripts/components/dashboard/dashboard.createApplication.js
@@ -56,9 +56,9 @@
     vm.isPending = false;
     vm.createApplication = createApplication;
     vm.isValid = isValid;
-
+   
     vm.selectConfig = {
-      optgroupField: 'undef',
+      optgroupField: 'prefixId',
       optgroupLabelField: 'title',
       maxItems: 1,
       valueField: 'id',
@@ -82,9 +82,9 @@
 
       function success(serverImages) {
         vm.serverImages = serverImages;
-
          serverImages.forEach(function (element, index) {
           var rang = vm.group.map(function(x) {return x.title; }).indexOf(serverImages[index].prefixEnv);
+          serverImages[index].prefixId = (serverImages[index].prefixId > 0)?serverImages[index].prefixId:-serverImages[index].prefixId;
           if(rang == -1) {
             vm.group.push({
               id: serverImages[index].prefixId,
