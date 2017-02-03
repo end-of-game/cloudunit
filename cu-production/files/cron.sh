99cbc47e8ddae00789e282e08f33d523424841d6
nicolas <zepouet@gmail.com>
2017-02-03 09:43:13 +0100

    Fix : Cron syntax

--- cu-production/files/cron.sh
+++ cu-production/files/cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STATUS=`docker inspect -f {{.State.Running}} cu-tomcat`
-if [ "$STATUS"=="false" ]; then
+if [ "$STATUS" == "false" ]; then
     cd /home/admincu/cloudunit/cu-compose
     /home/admincu/cloudunit/cu-compose/cu-docker-compose.sh with-elk
 fi
