cb12efd09c7b9fbedcef1f727fae1d1674a5da9f
Nicolas <zepouet@gmail.com>
2017-02-06 09:17:04 +0100

    Correct provisionning env vagrant

--- cu-vagrant/boot/provision.sh
+++ cu-vagrant/boot/provision.sh
@@ -7,12 +7,6 @@ export LANGUAGE=en_US.UTF-8
 
 readonly COMPOSE_VERSION=1.9.0
 
-set -x
-
-CU_USER=${1:-admincu}
-# CREATE ADMINCU USER admincu account
-useradd -m -s /bin/bash $CU_USER
-
 # PROVISION THE ENV
 apt-get install -y nmap
 apt-get install -y htop
@@ -20,20 +14,16 @@ apt-get install -y ncdu
 apt-get install -y git
 apt-get install -y haveged
 
-# CLONE CLOUDUNIT
-cd /home/$CU_USER && git clone https://github.com/Treeptik/cloudunit.git -b dev
-chown -R $CU_USER:$CU_USER /home/$CU_USER
-
 #==========================================================#
 sudo apt-get update
 sudo apt-get upgrade
 
-cp -f /home/$CU_USER/cloudunit/cu-vagrant/files/profile /home/$CU_USER/.profile
-source /home/$CU_USER/.profile
-cp -f /home/$CU_USER/cloudunit/cu-vagrant/files/hosts /etc/hosts
-cp -f /home/$CU_USER/cloudunit/cu-vagrant/files/environment /etc/environment
-cp -f /home/$CU_USER/cloudunit/cu-vagrant/files/.env.template /home/$CU_USER/cloudunit/cu-compose/.env
-cp -f /home/$CU_USER/cloudunit/cu-vagrant/files/.bashrc /home/$CU_USER/.bashrc
+cp -f /home/vagrant/cloudunit/cu-vagrant/files/profile /home/vagrant/.profile
+source /home/vagrant/.profile
+cp -f /home/vagrant/cloudunit/cu-vagrant/files/hosts /etc/hosts
+cp -f /home/vagrant/cloudunit/cu-vagrant/files/environment /etc/environment
+cp -f /home/vagrant/cloudunit/cu-vagrant/files/.env.template /home/vagrant/cloudunit/cu-compose/.env
+cp -f /home/vagrant/cloudunit/cu-vagrant/files/.bashrc /home/vagrant/.bashrc
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo cp -f cloudunit/cu-vagrant/files/sources.list /etc/apt/sources.list
@@ -55,8 +45,8 @@ sudo rm -f \
   /var/log/syslog     \
   /var/log/daemon.log \
   /var/log/upstart/docker.log \
-  /home/$CU_USER/.bash_history \
-  /var/mail/$CU_USER           \
+  /home/vagrant/.bash_history \
+  /var/mail/vagrant \
   || true
 
 #==========================================================#
@@ -70,19 +60,19 @@ sudo apt-get install -y docker-engine=1.12.6-0~ubuntu-$(lsb_release -sc)
 sudo apt-get install -y mysql-client
 
 #sudo apt-mark hold docker-engine
-sudo usermod -aG docker $CU_USER
+sudo usermod -aG docker vagrant
 
 # stop docker
 sudo service docker stop
 
 # configure docker
-sudo cp /home/$CU_USER/cloudunit/cu-vagrant/files/docker.no.secure.service  /etc/default/docker
+sudo cp /home/vagrant/cloudunit/cu-vagrant/files/docker.no.secure.service  /etc/default/docker
 
 # restart docker
 sudo service docker start
 
 # enable memory and swap accounting
-sudo cp /home/$CU_USER/cloudunit/cu-vagrant/files/grub /etc/default/grub
+sudo cp /home/vagrant/cloudunit/cu-vagrant/files/grub /etc/default/grub
 sudo update-grub
 
 # install Docker Compose
@@ -109,4 +99,4 @@ sudo mv docker-compose /usr/local/bin
 #sudo service docker stop
 #sudo service docker start
 
-cd /home/$CU_USER/cloudunit/cu-services && ./build-services.sh all
+cd /home/vagrant/cloudunit/cu-services && ./build-services.sh all
