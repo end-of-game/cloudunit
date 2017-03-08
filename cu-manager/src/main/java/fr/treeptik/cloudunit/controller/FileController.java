fa6271a12f7ed8ed687bfcad0fb23621bb236620
Nicolas <zepouet@gmail.com>
2017-03-08 17:37:16 +0100

    FIX : problem with unzip if need to override files

--- cu-manager/src/main/java/fr/treeptik/cloudunit/controller/FileController.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/controller/FileController.java
@@ -282,7 +282,7 @@ public class FileController {
 		if (FileUnit.tar().test(fileName)) {
 			command = "tar xvf " + realPath + " -C " + path;
 		} else if (FileUnit.zip().test(fileName)) {
-			command = "unzip " + realPath + " -o -d " + path;
+			command = "unzip -o " + realPath + " -d " + path;
 		} else {
 			throw new CheckException("Cannot decompress this file. Extension is not right : " + realPath);
 		}
--- cu-manager/src/test/resources/db.init-integration.sql
+++ cu-manager/src/test/resources/db.init-integration.sql
@@ -3,30 +3,34 @@ INSERT IGNORE INTO `Role` (`id`, `description`) VALUES
   (2, 'ROLE_USER');
 
 INSERT IGNORE INTO `Image`
-(`id`, `name`, `path`, `displayName`, `prefixEnv`, `imageType`, `managerName`, `imageSubType`) VALUES
-  (10, 'tomcat-6', 'cloudunit/tomcat-6', 'Tomcat 6.0.45', 'tomcat', 'server', '', 'APPSERVER'),
-  (11, 'tomcat-7', 'cloudunit/tomcat-7', 'Tomcat 7.0.70', 'tomcat', 'server', '',  'APPSERVER'),
-  (12, 'tomcat-8', 'cloudunit/tomcat-8', 'Tomcat 8.0.39', 'tomcat', 'server', '', 'APPSERVER'),
-  (13, 'tomcat-85', 'cloudunit/tomcat-85', 'Tomcat 8.5.9', 'tomcat', 'server', '', 'APPSERVER'),
-  (14, 'tomcat-9', 'cloudunit/tomcat-9', 'Tomcat 9.0.0.M15', 'tomcat', 'server', '', 'APPSERVER'),
-  (20, 'mysql-5-5', 'cloudunit/mysql-5-5', 'MySQL 5.5.49', 'mysql', 'module', 'phpmyadmin', 'DATABASE'),
-  (21, 'mysql-5-6', 'cloudunit/mysql-5-6', 'MySQL 5.6.30', 'mysql', 'module', 'phpmyadmin', 'DATABASE'),
-  (22, 'mysql-5-7', 'cloudunit/mysql-5-7', 'MySQL 5.7.12', 'mysql', 'module', 'phpmyadmin', 'DATABASE'),
-  (30, 'mongo-2-6', 'cloudunit/mongo-2-6', 'Mongo 2.6', 'mongo', 'module', 'mms', 'DATABASE'),
-  (40, 'redis-3-2', 'cloudunit/redis-3-2', 'Redis 3.2', 'redis', 'module', 'redmin', 'DATAGRID'),
-  (50, 'postgresql-9-3', 'cloudunit/postgresql-9-3', 'PostgreSQL 9.3', 'postgresql', 'module', 'phppgadmin', 'DATABASE'),
-  (51, 'postgresql-9-4', 'cloudunit/postgresql-9-4', 'PostgreSQL 9.4', 'postgresql', 'module', 'phppgadmin', 'DATABASE'),
-  (52, 'postgresql-9-5', 'cloudunit/postgresql-9-5', 'PostgreSQL 9.5', 'postgresql', 'module', 'phppgadmin', 'DATABASE'),
-  (53, 'postgis-2-2', 'cloudunit/postgis-2-2', 'PostGIS 2.2.1 (9.4)', 'postgresql', 'module', 'phppgadmin','DATABASE'),
-  (60, 'wildfly-8', 'cloudunit/wildfly-8', 'WildFly 8.2.1', 'wildfly', 'server', '','APPSERVER'),
-  (61, 'wildfly-9', 'cloudunit/wildfly-9', 'WildFly 9.0.2', 'wildfly', 'server', '', 'APPSERVER'),
-  (62, 'wildfly-10', 'cloudunit/wildfly-10', 'WildFly 10.0.0', 'wildfly', 'server', '','APPSERVER'),
-  (70, 'fatjar', 'cloudunit/fatjar', 'FatJar', 'fatjar', 'server', '', 'STANDALONE'),
-  (80, 'apache-2-2', 'cloudunit/apache-2-2', 'Apache 2.2.22', 'apache', 'server', '', 'WEBSERVER'),
-  (90, 'activemq-5.13', 'cloudunit/activemq-5.13', 'ActiveMQ 5.13.2', 'activemq', 'module', '', 'MESSAGE_BROKER'),
-  (100, 'rabbitmq-3.6', 'cloudunit/rabbitmq-3.6', 'RabbitMQ 3.6.5-1', 'rabbitmq', 'module', '', 'MESSAGE_BROKER'),
-  (110, 'elasticsearch-2.4', 'cloudunit/elasticsearch-2.4', 'Elasticsearch 2.4', 'elasticsearch', 'module', '', 'SEARCH_ENGINE'),
-  (120, 'nginx-1.10', 'cloudunit/nginx-1.10', 'Nginx 1.10', 'nginx', 'server', 'server', 'WEBSERVER');
+    (`id`, `name`, `path`, `displayName`, `prefixEnv`, `imageType`, `managerName`, `imageSubType`, `isEnable`) VALUES
+  (10, 'tomcat-6', 'cloudunit/tomcat-6', 'Tomcat 6.0.45', 'tomcat', 'server', '', 'APPSERVER', true),
+  (11, 'tomcat-7', 'cloudunit/tomcat-7', 'Tomcat 7.0.70', 'tomcat', 'server', '',  'APPSERVER', true),
+  (12, 'tomcat-8', 'cloudunit/tomcat-8', 'Tomcat 8.0.39', 'tomcat', 'server', '', 'APPSERVER', true),
+  (13, 'tomcat-85', 'cloudunit/tomcat-85', 'Tomcat 8.5.9', 'tomcat', 'server', '', 'APPSERVER', true),
+  (14, 'tomcat-9', 'cloudunit/tomcat-9', 'Tomcat 9.0.0.M15', 'tomcat', 'server', '', 'APPSERVER', true),
+  (20, 'mysql-5-5', 'cloudunit/mysql-5-5', 'MySQL 5.5.49', 'mysql', 'module', 'phpmyadmin', 'DATABASE', true),
+  (21, 'mysql-5-6', 'cloudunit/mysql-5-6', 'MySQL 5.6.30', 'mysql', 'module', 'phpmyadmin', 'DATABASE', true),
+  (22, 'mysql-5-7', 'cloudunit/mysql-5-7', 'MySQL 5.7.12', 'mysql', 'module', 'phpmyadmin', 'DATABASE', true),
+  (30, 'mongo-2-6', 'cloudunit/mongo-2-6', 'Mongo 2.6', 'mongo', 'module', 'mms', 'DATABASE', true),
+  (40, 'redis-3-2', 'cloudunit/redis-3-2', 'Redis 3.2', 'redis', 'module', 'redmin', 'DATAGRID', true),
+  (50, 'postgresql-9-3', 'cloudunit/postgresql-9-3', 'PostgreSQL 9.3', 'postgresql', 'module', 'phppgadmin', 'DATABASE', true),
+  (51, 'postgresql-9-4', 'cloudunit/postgresql-9-4', 'PostgreSQL 9.4', 'postgresql', 'module', 'phppgadmin', 'DATABASE', true),
+  (52, 'postgresql-9-5', 'cloudunit/postgresql-9-5', 'PostgreSQL 9.5', 'postgresql', 'module', 'phppgadmin', 'DATABASE', true),
+  (53, 'postgis-2-2', 'cloudunit/postgis-2-2', 'PostGIS 2.2.1 (9.4)', 'postgresql', 'module', 'phppgadmin','DATABASE', true),
+  (60, 'wildfly-8', 'cloudunit/wildfly-8', 'WildFly 8.2.1', 'wildfly', 'server', '','APPSERVER', true),
+  (61, 'wildfly-9', 'cloudunit/wildfly-9', 'WildFly 9.0.2', 'wildfly', 'server', '', 'APPSERVER', true),
+  (62, 'wildfly-10', 'cloudunit/wildfly-10', 'WildFly 10.0.0', 'wildfly', 'server', '','APPSERVER', true),
+  (70, 'fatjar', 'cloudunit/fatjar', 'FatJar', 'fatjar', 'server', '', 'STANDALONE', true),
+  (80, 'apache-2-2', 'cloudunit/apache-2-2', 'Apache 2.2.22', 'apache', 'server', '', 'WEBSERVER', true),
+  (90, 'activemq-5.13', 'cloudunit/activemq-5.13', 'ActiveMQ 5.13.2', 'activemq', 'module', '', 'MESSAGE_BROKER', true),
+  (100, 'rabbitmq-3.6', 'cloudunit/rabbitmq-3.6', 'RabbitMQ 3.6.5-1', 'rabbitmq', 'module', '', 'MESSAGE_BROKER', true),
+  (110, 'elasticsearch-2.4', 'cloudunit/elasticsearch-2.4', 'Elasticsearch 2.4', 'elasticsearch', 'module', '', 'SEARCH_ENGINE', true),
+  (120, 'nginx-1.10', 'cloudunit/nginx-1.10', 'Nginx 1.10', 'nginx', 'server', 'server', 'WEBSERVER', true),
+  (130, 'vertx33-ruby', 'cloudunit/vertx33-ruby', 'Vertx 3.3 Ruby', 'vertx', 'server', '', 'STANDALONE', true),
+  (131, 'vertx33-js', 'cloudunit/vertx33-js', 'Vertx 3.3 Javascript', 'vertx', 'server', '', 'STANDALONE', true),
+  (132, 'vertx33-groovy', 'cloudunit/vertx33-groovy', 'Vertx 3.3 Groovy', 'vertx', 'server', '', 'STANDALONE', true),
+  (133, 'vertx33-java', 'cloudunit/vertx33-java', 'Vertx 3.3 Java', 'vertx', 'server', '', 'STANDALONE', true);
 
 INSERT IGNORE INTO `Image_moduleEnvironmentVariables` (`moduleEnvironmentVariables`,`moduleEnvironmentVariables_KEY`,`Image_id`)
 VALUES
