c7b99355b4328269da1013f84016f4eba4949985
William Bartlett <wbartlett@treeptik.fr>
2017-03-22 11:28:13 +0100

    Clean up UI

--- cu-manager/cu-manager-ui/src/main/java/fr/treeptik/cloudunit/ui/CloudUnitManagerUiApplication.java
+++ cu-manager/cu-manager-ui/src/main/java/fr/treeptik/cloudunit/ui/CloudUnitManagerUiApplication.java
@@ -1,15 +1,14 @@
-package fr.treeptik.cloudunit;
+package fr.treeptik.cloudunit.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class CuManagerUiApplication {
+public class CloudUnitManagerUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CuManagerUiApplication.class, args);
+		SpringApplication.run(CloudUnitManagerUiApplication.class, args);
 	}
 }
