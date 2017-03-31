c02949903afe3aaa4f188677a7fd8a5c1e7bbbcd
William Bartlett <wbartlett@treeptik.fr>
2017-03-31 14:19:41 +0200

    Harmonize Tomcat Dockerfiles

--- cu-services/images/tomcat-9/docker-entrypoint.sh
+++ cu-services/images/tomcat-9/docker-entrypoint.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-CU_USER=$2
-CU_PASSWORD=$3
-
-if [[ ! -z "$CU_USER" ]] && [[ ! -z "$CU_PASSWORD" ]]
+if [[ ! -z "$TOMCAT_USER" ]] && [[ ! -z "$TOMCAT_PASSWORD" ]]
 then
-    PATTERN_USER="s/CU_USER/$CU_USER/g"
-    PATTERN_PASSWD="s/CU_PASSWORD/$CU_PASSWORD/g"
-    sed -i $PATTERN_USER /opt/cloudunit/tomcat/conf/tomcat-users.xml
-    sed -i $PATTERN_PASSWD /opt/cloudunit/tomcat/conf/tomcat-users.xml
-fi
+    cat > conf/tomcat-users.xml <<EOF
+<?xml version='1.0' encoding='utf-8'?>
 
-# if $APPLICATIVE_MONITORING doesn't exist or is equals to 1
-if [ -z "$APPLICATIVE_MONITORING" ] || [ "$APPLICATIVE_MONITORING" -eq 1 ]; then
-	/opt/cloudunit/monitoring-agents/metricbeat/metricbeat -c /opt/cloudunit/monitoring-agents/metricbeat/conf.d/nginx.yml&
+<tomcat-users
+    xmlns="http://tomcat.apache.org/xml"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
+    version="1.0">
+    <role rolename="manager-gui"/>
+    <role rolename="manager-script"/>
+    <role rolename="manager-jmx"/>
+    <user username="$TOMCAT_USER" password="$TOMCAT_PASSWORD" roles="manager-gui,manager-script,manager-jmx"/>
+</tomcat-users>
+EOF
 fi
 
 # if $JMX_MONITORING doesn't exist or is equals to 1
 if [ -z "$JMX_MONITORING" ] || [ "$JMX_MONITORING" -eq 1 ]; then
-    JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/cloudunit/tomcat/lib/jmxtrans-agent-1.2.5-SNAPSHOT-jar-with-dependencies.jar=/opt/cloudunit/conf/jmxtrans-agent.xml"
-fi
-
-if [[ $1 == "run" ]]; then
-  exec catalina.sh "run"
+    JAVA_OPTS="$JAVA_OPTS -javaagent:lib/jmxtrans-agent-1.2.5-SNAPSHOT-jar-with-dependencies.jar=conf/jmxtrans-agent.xml"
 fi
 
 exec "$@"
+
