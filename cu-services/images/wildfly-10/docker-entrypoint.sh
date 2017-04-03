f74d07dbd7e0eed537a54d00268e0e94b579dbbe
William Bartlett <wbartlett@treeptik.fr>
2017-04-03 21:10:36 +0200

    Rework Wildfly images

--- cu-services/images/wildfly-10/docker-entrypoint.sh
+++ cu-services/images/wildfly-10/docker-entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-CU_USER=$2
-CU_PASSWORD=$3
-
-if [[ ! -z "$CU_USER" ]] && [[ ! -z "$CU_PASSWORD" ]]
+if [[ ! -z "$WILDFLY_USER" ]] && [[ ! -z "$WILDFLY_PASSWORD" ]]
 then
-    /opt/cloudunit/wildfly/bin/add-user.sh --silent=true $CU_USER $CU_PASSWORD
-fi
-
-if [[ $1 == "run" ]]; then
-  /opt/cloudunit/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+    add-user.sh -s $WILDFLY_USER $WILDFLY_PASSWORD
 fi
 
 exec "$@"
