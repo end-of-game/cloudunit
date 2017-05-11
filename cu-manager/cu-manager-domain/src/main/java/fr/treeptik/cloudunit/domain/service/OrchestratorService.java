2fcefc2150a457a8141fb8e0f2288272cc7640f1
Michaël GRILLET <m.grillet@treeptik.fr>
2017-05-11 15:36:08 +0200

    Remove use of repository to get images in manager domain

--- cu-manager/cu-manager-domain/src/main/java/fr/treeptik/cloudunit/domain/service/OrchestratorService.java
+++ cu-manager/cu-manager-domain/src/main/java/fr/treeptik/cloudunit/domain/service/OrchestratorService.java
@@ -1,10 +1,13 @@
 package fr.treeptik.cloudunit.domain.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import fr.treeptik.cloudunit.domain.core.Image;
 
 public interface OrchestratorService {
 	List<Image> findAllImages();
+
+	Optional<Image> findImageByName(String imageName);
 
 }
