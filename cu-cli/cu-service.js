298eb54a43d64b67c7aceb68cdf5f31f386124b9
Michaël GRILLET <m.grillet@treeptik.fr>
2017-05-26 11:48:02 +0200

    Fix deploy command

--- cu-cli/cu-service.js
+++ cu-cli/cu-service.js
@@ -4,6 +4,7 @@ const program = require('commander');
 const chalk = require('chalk');
 const columnify = require('columnify');
 const fs =  require("fs");
+const request = require('request');
 
 const client = require('./lib/client');
 const out = require('./lib/out');
@@ -73,17 +74,20 @@ program
   .action(function(app, service, contextPath, filePath) {
     var file = fs.createReadStream(filePath);
     client.applicationsFile
-    .follow('cu:applications[name:'+app+']', 'cu:services', 'cu:services[name:'+service+']', 'cu:deployments', 'cu:deployments[contextPath:'+contextPath+']')
-    .put({ 'file': file }, function (error, response) {
-      if (error) {
-        out.error('ER Couldn\'t deploy the archive: '+error);
+    .follow('cu:applications[name:'+app+']', 'cu:services', 'cu:services[name:'+service+']', 'cu:deployments')
+    .getUrl(function(err, url) {
+      url = url + '/' + contextPath;
+      request({ url: url, method: 'PUT', formData : { 'file' : file }}, function(error, response) {
+        if (error) {
+        out.error('Couldn\'t deploy the archive: '+error);
         return;
       }
-      if (response.statusCode != 201) {
+      if (response.statusCode != 200) {
         out.error('Couldn\'t deploy the archive: '+response.body);
         return;
       }
       out.info('War deployed');
+      });
     });
   });
 
