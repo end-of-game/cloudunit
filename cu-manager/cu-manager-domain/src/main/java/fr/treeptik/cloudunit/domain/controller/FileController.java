c256c66c1aa51e27f6e2dc6a7e7bee5a8bf9b2fa
Michaël GRILLET <m.grillet@treeptik.fr>
2017-06-02 14:19:31 +0200

    Remove testing method in file controller

--- cu-manager/cu-manager-domain/src/main/java/fr/treeptik/cloudunit/domain/controller/FileController.java
+++ cu-manager/cu-manager-domain/src/main/java/fr/treeptik/cloudunit/domain/controller/FileController.java
@@ -5,15 +5,11 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.mongodb.gridfs.GridFSDBFile;
 
@@ -31,13 +27,6 @@ public class FileController {
 		GridFSDBFile file = storageService.findById(fileId);
 		response.setHeader("Content-Disposition", String.format("attachment; filename=%s", file.getFilename()));
 		FileCopyUtils.copy(file.getInputStream(), response.getOutputStream());
-	}
-	
-	// TODO just to test, to remove
-	@PostMapping
-	public ResponseEntity<?> upload(@RequestPart MultipartFile file) {
-		String id = storageService.store(file);
-		return ResponseEntity.ok(id);
 	}
 
 }
