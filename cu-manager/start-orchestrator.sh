67e26953ea7b3b25af7c4b921dea30f383038ea3
Michaël GRILLET <m.grillet@treeptik.fr>
2017-06-13 16:31:35 +0200

    Test Travis

--- cu-manager/start-orchestrator.sh
+++ cu-manager/start-orchestrator.sh
@@ -3,7 +3,7 @@ mvn spring-boot:run -Plocal -pl cu-docker-orchestrator > /dev/null 2>&1 &
 PID=$!
 echo $PID > .pid
 
-for i in {1..30}
+for i in {1..60}
 do
     STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8081)
     if [ $STATUS -eq 200 ]; then
