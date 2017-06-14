3bee002464f0e8c0d04e50143a6434a94bd9f95d
Michaël GRILLET <m.grillet@treeptik.fr>
2017-06-14 16:24:01 +0200

    Test Travis

---
+++ update-docker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" > /etc/apt/sources.list.d/docker.list'
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+sudo apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update
+sudo apt-get -y install "docker-engine=1.13.1-0~ubuntu-$(lsb_release -cs)"
