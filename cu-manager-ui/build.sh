788eb6bbdc5ea09ecd919ad5795c3ebea31ec818
Michaël GRILLET <m.grillet@treeptik.fr>
2017-06-15 17:42:22 +0200

    Move front-end app to reverse-proxy project

--- cu-manager-ui/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\n\n### npm install ###"
-npm prune -dd
-npm install -dd
-echo -e "\n\n### bower install ###"
-bower install --allow-root
-echo -e "\n\n### grunt build ###"
-grunt build
