0661a5ed4a57567fb4da48f5981c5290aa0fc342
IsmaStormZ <charifou.ismael@gmail.com>
2017-06-23 09:32:12 +0200

    add index.js

---
+++ testing-cloudUnit/_site/index.js
@@ -0,0 +1,63 @@
+/*
+ Date: 21th June
+ Author: IsmaStormZ
+ */
+
+const http = require('http');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const sendmail = require('sendmail')({
+    logger: {
+        debug: console.log,
+        info: console.info,
+        warn: console.warn,
+        error: console.error
+    }});
+
+const app = express();
+const port = Number(process.env.PORT || 5000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use('/pages', express.static(__dirname));
+
+//Home page
+
+app.get('/', (req,res) => {
+    res.redirect('index.html');
+console.log('CloudUnit reading console log ...' + req.url);
+});
+
+// Create reusable transporter object using the default SMTP transport and sending mail function
+
+app.post('/contact', (req, res) => {
+    if(req.body.setName == "" || req.body.setEmail == "") {     //check that the requested fields are filled in.
+    res.send("Error: Name & Email should not be blank");    //Message if issue in field
+    return false;
+}
+
+// setup email data
+let mailOptions = {
+    from: "cloudunit@treeptik.com",                           // sender address
+    to: "Treeptik mail - <onifuerte@gmail.com>",        // list of receivers
+    subject: 'Cloudunit new test.',
+    html: "<b>" + "Name : " + req.body.setName + "<b>" + "<br>" + "Mail : " + req.body.setEmail   // name to form in index.html
+};
+
+sendmail(mailOptions, (err, reply) => {
+    console.log(err);
+console.dir(reply);
+});
+res.end();
+});
+
+//Starting server
+
+const server = http.createServer(app).listen(port, () => {
+        console.log("Server Running on 127.0.0.1 : " + port);
+});
+
