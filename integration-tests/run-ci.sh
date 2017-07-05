e4a3d74a61042fa5d40bcd75bbe87efff131e1c6
Nicolas <zepouet@gmail.com>
2017-07-05 20:31:28 +0200

    Change build options for jenkins env

--- integration-tests/run-ci.sh
+++ integration-tests/run-ci.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+export CLOUDUNIT_HOME=/home/admincu/.jenkins/workspace/cloudunit
 source /etc/environment
-#cd ~/cloudunit && git pull
-#cd ~/cloudunit/cu-services && echo yes | ./die-hard.sh
-#cd ~/cloudunit/cu-services && ./build-services.sh fatjar
+#cd cloudunit && git pull
+cd $CLOUDUNIT_HOME/cu-services && echo yes | ./die-hard.sh
+cd $CLOUDUNIT_HOME/cu-services && ./build-services.sh fatjar
 docker network create skynet
-cd ~/cloudunit/cu-compose && docker-compose -f docker-compose.elk.yml up -d
-cd ~/cloudunit/integration-tests && docker-compose kill && docker-compose rm -f && docker-compose up -d
-cd ~/cloudunit && /home/admincu/software/maven/bin/mvn clean install -DskipTests
-cd ~/cloudunit/cu-manager && /home/admincu/software/maven/bin/mvn test "-Dtest=Tomcat8ApplicationControllerTestIT"
+cd $CLOUDUNIT_HOME/cu-compose && docker-compose -f docker-compose.elk.yml up -d
+cd $CLOUDUNIT_HOME/integration-tests && docker-compose kill && docker-compose rm -f && docker-compose up -d
+cd $CLOUDUNIT_HOME && /home/admincu/software/maven/bin/mvn clean install -DskipTests
+cd $CLOUDUNIT_HOME/cu-manager && /home/admincu/software/maven/bin/mvn test "-Dtest=Tomcat8ApplicationControllerTestIT"
 
