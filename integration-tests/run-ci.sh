4658e483ee2abfb05d0f005fe1a768598c9b4c94
Nicolas <zepouet@gmail.com>
2017-07-05 18:43:50 +0200

    Change build options for jenkins env

--- integration-tests/run-ci.sh
+++ integration-tests/run-ci.sh
@@ -5,7 +5,7 @@ docker rm -f $(docker ps -aq)
 docker run --name cu-monitoring-agents -d cloudunit/elk-monitoring-agents
 cd ~/cloudunit && git pull
 cd ~/cloudunit/cu-services && echo yes | ./die-hard.sh
-cd ~/cloudunit/cu-services && ./build-services.sh all
+cd ~/cloudunit/cu-services && ./build-services.sh tomcat
 cd ~/cloudunit/cu-compose && docker-compose -f docker-compose.elk.yml up -d
 cd ~/cloudunit/integration-tests && docker-compose kill && docker-compose rm -f && docker-compose up -d
 cd ~/cloudunit && mvn clean install -DskipTests
