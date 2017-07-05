dbb75c939b82f0d3af0d57e65a0ad1d2b68c8c4a
Nicolas <zepouet@gmail.com>
2017-07-05 21:44:19 +0200

    Change build options for jenkins env

--- integration-tests/run-ci.sh
+++ integration-tests/run-ci.sh
@@ -4,10 +4,10 @@ export CLOUDUNIT_HOME=/home/admincu/.jenkins/workspace/cloudunit
 source /etc/environment
 #cd cloudunit && git pull
 cd $CLOUDUNIT_HOME/cu-services && echo yes | ./die-hard.sh
-cd $CLOUDUNIT_HOME/cu-services && ./build-services.sh tomcat
+cd $CLOUDUNIT_HOME/cu-services && ./build-services.sh all
 docker network create skynet
 cd $CLOUDUNIT_HOME/cu-compose && docker-compose -f docker-compose.elk.yml up -d
 cd $CLOUDUNIT_HOME/integration-tests && docker-compose kill && docker-compose rm -f && docker-compose up -d
 cd $CLOUDUNIT_HOME && /home/admincu/software/maven/bin/mvn clean install -DskipTests
-cd $CLOUDUNIT_HOME/cu-manager && /home/admincu/software/maven/bin/mvn test "-Dtest=Tomcat8ApplicationControllerTestIT"
+cd $CLOUDUNIT_HOME/cu-manager && /home/admincu/software/maven/bin/mvn test "-Dtest=*IT"
 
