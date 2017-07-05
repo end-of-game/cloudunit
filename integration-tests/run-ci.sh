4bf1aa9acbbb7730f0d3968692d0a1323214d63a
Nicolas <zepouet@gmail.com>
2017-07-05 21:37:20 +0200

    Change build options for jenkins env

--- integration-tests/run-ci.sh
+++ integration-tests/run-ci.sh
@@ -4,7 +4,7 @@ export CLOUDUNIT_HOME=/home/admincu/.jenkins/workspace/cloudunit
 source /etc/environment
 #cd cloudunit && git pull
 cd $CLOUDUNIT_HOME/cu-services && echo yes | ./die-hard.sh
-cd $CLOUDUNIT_HOME/cu-services && ./build-services.sh fatjar
+cd $CLOUDUNIT_HOME/cu-services && ./build-services.sh tomcat
 docker network create skynet
 cd $CLOUDUNIT_HOME/cu-compose && docker-compose -f docker-compose.elk.yml up -d
 cd $CLOUDUNIT_HOME/integration-tests && docker-compose kill && docker-compose rm -f && docker-compose up -d
