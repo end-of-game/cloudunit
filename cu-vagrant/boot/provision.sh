3f4faaaf01a89a5c36c01c56e0ba219060d7572f
tkrief <t.krief@treeptik.fr>
2017-07-06 11:07:23 +0200

    Update Docker version to 17.06.0 CE

--- cu-vagrant/boot/provision.sh
+++ cu-vagrant/boot/provision.sh
@@ -56,7 +56,25 @@ sudo rm -f \
 #
 sudo apt-get update
 sudo apt-get install -y linux-image-extra-$(uname -r)
-sudo apt-get install -y docker-engine=1.12.6-0~ubuntu-$(lsb_release -sc)
+
+# Install Docker-CE
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce=17.06.0~ce-0~ubuntu
+
 sudo apt-get install -y mysql-client
 
 #sudo apt-mark hold docker-engine
