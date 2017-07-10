f5b1df782ca9773d2e1ee0d831fffbd24050e879
IsmaStormZ <charifou.ismael@gmail.com>
2017-07-10 15:49:35 +0200

    modified index.html to connection with node app

--- testing-cloudunit/_site/index.js
+++ testing-cloudunit/_site/index.js
@@ -3,10 +3,10 @@
  Author: IsmaStormZ
  */
 
-const http = require('http');               //To use the HTTP server and client one must require('http')
+const http = require('http');               // To use the HTTP server and client one must require('http')
 const express = require('express');         // express is a minimalist framework for node
 const bodyParser = require('body-parser');  // body-parser extracts entire body portion of an request stream and exposes it on req.body
-const sendmail = require('sendmail')({      //Send mail without SMTP server
+const sendmail = require('sendmail')({      // Send mail without SMTP server
     logger: {
         debug: console.log,
         info: console.info,
@@ -18,18 +18,20 @@ const app = express();
 const port = Number(process.env.PORT || 3000); // set the environment variable PORT to tell your web server what port to listen on, whatever is in the environment variable PORT, or 3000 if there's nothing there.
 
 //Parsed the text as JSON
+
 app.use(bodyParser.json());
 
-//Parses the text as URL encoded data
+// Parses the text as URL encoded data
+
 app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-//express.static permit to use any static files(js, html, css) related to the static directory
+// express.static permit to use any static files(js, html, css) related to the static directory
 
 app.use('/', express.static(__dirname));
 
-//Home page
+// Home page
 
 app.get('/', (req,res) => {
     res.send('Hello from Docker\n');
@@ -41,12 +43,13 @@ app.get('/', (req,res) => {
 
 app.post('/contact', (req, res) => {
     console.log(JSON.stringify(req.body));
-    if(req.body.setName == "" || req.body.setEmail == "") {     //check that the requested fields are filled in.
-        res.send("Error: Name & Email should not be blank");        //Message if issue in field
+    if(req.body.setName == "" || req.body.setEmail == "") {     // check that the requested fields are filled in.
+        res.send("Error: Name & Email should not be blank");        // Message if issue in field
         return false;
     }
 
-//setup email data
+// setup email data
+
     let mailOptions = {
         from: "cloudunit@treeptik.com",                         // sender address
         to: "Treeptik mail - <onifuerte@gmail.com>",            // list of receivers
@@ -54,17 +57,20 @@ app.post('/contact', (req, res) => {
         html: "<b>" + "Name : " + req.body.setName + "<b>" + "<br>" + "Mail : " + req.body.setEmail   // name to form in index.html
     };
 
-//In case of error
+// In case of error
+
     sendmail(mailOptions, (err, reply) => {
         console.log(err);
         console.dir(reply);
     });
 
-//redirect to confirm page
+// redirect to confirm page
+
     res.redirect("contact-success.html");
 });
 
-//Starting server
+// Starting server
+
 const server = http.createServer(app).listen(port, () => {
     console.log("Server Running on 127.0.0.1 : " + port);
 });
