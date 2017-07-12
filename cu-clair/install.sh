06c8e11cb8bed5dc9d4d8166b9dd3328455ac43a
Nicolas <zepouet@gmail.com>
2017-07-12 17:40:53 +0200

    Test Clair. Not right now

--- cu-clair/install.sh
+++ cu-clair/install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -ue
-##
 
 ## CoreOS Clair
-echo ">>> Installing CoreOS Clair"
-docker-compose up -d
+echo "Installing CoreOS Clair"
+docker-compose pull
 
 ## clairctl 
-echo ">>> Installing clairctl"
+echo "Installing clairctl"
 PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 if [[ "$(uname -m)" == *"64"* ]]; then ARCH="amd64"; else ARCH="386"; fi
@@ -16,6 +15,5 @@ echo $ARCH
 curl -o /usr/local/bin/clairctl "https://s3.amazonaws.com/clairctl/latest/clairctl-$PLATFORM-$ARCH"
 chmod +x /usr/local/bin/clairctl
 
-clairctl health --log-level debug
-
 exit 0
+
