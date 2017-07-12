3d931d3f27600493832e0470daaee8fe331cc58a
Nicolas <zepouet@gmail.com>
2017-07-12 11:43:18 +0200

    Remove reference to dnsmasq to use only xip.io

--- cu-manager/src/main/java/fr/treeptik/cloudunit/controller/HomepageController.java
+++ cu-manager/src/main/java/fr/treeptik/cloudunit/controller/HomepageController.java
@@ -15,22 +15,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/homepage")
 public class HomepageController {
 
-    @Value("#{environment.CU_JENKINS_DOMAIN ?: 'jenkins.cloudunit.dev'}")
+    @Value("#{environment.CU_JENKINS_DOMAIN ?: 'jenkins.192.168.50.4.xip.io'}")
     private String jenkins;
 
-    @Value("#{environment.CU_GITLAB_DOMAIN ?: 'gitlab.cloudunit.dev'}")
+    @Value("#{environment.CU_GITLAB_DOMAIN ?: 'gitlab.192.168.50.4.xip.io'}")
     private String gitlab;
 
-    @Value("#{environment.CU_NEXUS_DOMAIN ?: 'nexus.cloudunit.dev'}")
+    @Value("#{environment.CU_NEXUS_DOMAIN ?: 'nexus.192.168.50.4.xip.io'}")
     private String nexus;
 
-    @Value("#{environment.CU_KIBANA_DOMAIN ?: 'kibana.cloudunit.dev'}")
+    @Value("#{environment.CU_KIBANA_DOMAIN ?: 'kibana.192.168.50.4.xip.io'}")
     private String kibana;
 
-    @Value("#{environment.CU_SONAR_DOMAIN ?: 'sonar.cloudunit.dev'}")
+    @Value("#{environment.CU_SONAR_DOMAIN ?: 'sonar.192.168.50.4.xip.io'}")
     private String sonar;
 
-    @Value("#{environment.CU_MATTERMOST_DOMAIN ?: 'mattermost.cloudunit.dev'}")
+    @Value("#{environment.CU_MATTERMOST_DOMAIN ?: 'mattermost.192.168.50.4.xip.io'}")
     private String mattermost;
 
     @RequestMapping(value = "/friends", method = RequestMethod.GET)
